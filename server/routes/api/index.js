148cc136755dbf2e500f52549f5b2dd3dfbc3b91
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2016-12-02 13:34:21 +0530

    reputation request from express server

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -7,6 +7,8 @@
 
 const apiRouter = require('express').Router();
 const db = require('../../config/db');
+import axios from 'axios';
+import {vendastaApiKey} from '../../../config.json'
 
 const MAX_RESULTS = 100;
 
@@ -17,5 +19,18 @@ apiRouter.get('/availabilities', (req, res, next) => {
     res.send(results);
   });
 });
+
+apiRouter.get('/reputation', (req, res, next) => {
+  console.log('GET reputation');
+  axios.post('https://reputation-intelligence-api.vendasta.com/api/v2/listing/getStats/?apiKey=' + vendastaApiKey + '&apiUser=CARU', {
+      customerIdentifier: req.params.cust_id || "UNIQUE_CUSTOMER_IDENTIFIER"
+    })
+    .then(function (response) {
+      return res.send(response.data);
+    })
+    .catch(function (error) {
+      return res.sendStatus(404)
+    });
+})
 
 module.exports = apiRouter;
