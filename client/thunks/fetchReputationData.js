be6615a1914eaeef3cafb3abe306ff1f63abcd1a
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2016-12-02 13:38:30 +0530

    reputation api call from client

--- client/thunks/fetchReputationData.js
+++ client/thunks/fetchReputationData.js
@@ -1,45 +1,11 @@
 
 import { fetchReputationSuccess } from '../actions/reputation';
 
-// TODO use fetch after access control is allowed, or proxy through node
-// export function fetchReputation (dispatch) {
-//   return function() {
-//     return fetch(REPUTATION_URL, {
-//       method: 'POST',
-//       body: JSON.stringify({
-//         customerIdentifier: "UNIQUE_CUSTOMER_IDENTIFIER"
-//       }),
-//       mode: 'cors'
-//     }).then(response => response.json())
-//     .then(data => dispatch(fetchReputationSuccess(data)))
-//   }
-// };
+export default function fetchReputation () {
+  return function(dispatch) {
+    return fetch('/api/reputation', {
+    }).then(response => response.json())
+    .then(data => dispatch(fetchReputationSuccess(data)))
+  }
+};
 
-
-export default function fetchReputation() {
-  return (dispatch, getState) => {
-    return setTimeout(() => {
-      return dispatch(fetchReputationSuccess({
-        version: '2.0',
-        data: {
-          sourcesNotFound: 16,
-          sourcesFound: 2,
-          listingScore: 12,
-          listingPointScore: {
-            industryAverage: 398,
-            industryLeadersAverage: 679,
-            pointScore: 217,
-          },
-          
-          citationsFound: 18,
-          sourcesFoundWithErrors: 2,
-          accuracyScore: 6.6001165234374994,
-        },
-        
-        requestId: '',
-        responseTime: 1544,
-        statusCode: 200,
-      }));
-    }, 1000);
-  };
-}
