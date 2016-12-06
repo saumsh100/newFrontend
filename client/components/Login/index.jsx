a473ef4a08167c96d28471fd8a5b22c463afd454
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2016-12-07 01:54:40 +0530

    login form and login route

---
+++ client/components/Login/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './styles.scss';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+
+export default class Login extends React.Component {
+  render() {
+    return (
+      <Form className={styles.loginForm}>
+        <FormGroup>
+          <Label for="email">Email</Label>
+          <Input type="email" name="email" id="email" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="password">Password</Label>
+          <Input type="password" name="password" id="password" />
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+    );
+  }
+}
