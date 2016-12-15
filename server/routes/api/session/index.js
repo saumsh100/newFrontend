32fdc73649e97465e0b7bd58e4eccc64ac80fb76
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2016-12-16 02:37:51 +0530

    add jwt and sign tokens

--- server/routes/api/session/index.js
+++ server/routes/api/session/index.js
@@ -1,14 +1,15 @@
 const sessionRouter = require('express').Router();
 const User = require('../../../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 sessionRouter.post('/', function(req, res, next) {
-  User.filter({username: req.body.username}).run().then(function(users) {
+  return User.filter({username: req.body.username}).run().then(function(users) {
     if (!users.length) {
       return next({status: 401})
     }
     const user = users[0];
-    bcrypt.compare(req.body.password, user.password, function (err, match) {
+    return bcrypt.compare(req.body.password, user.password, function (err, match) {
       if (err) {
         return next({status: 500});
       }
@@ -16,10 +17,18 @@ sessionRouter.post('/', function(req, res, next) {
         return next({status: 401});
       }
 
-      return res.end()
+      return jwt.sign({
+        data: {
+          username: user.username
+        },
+      }, 'notsosecret', { expiresIn: '1h' }, function(err, token) {
+        if (err) {
+          return next({status: 500});
+        }
+        return res.json({token})
+      });
     })
   });
-
 });
 
 sessionRouter.delete('/', (req, res, next) => {
