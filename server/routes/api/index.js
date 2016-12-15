cbf7f271802a7000bdde5e42c0f9becae6f0a931
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2016-12-16 04:17:55 +0530

    add jwt middleware for auth

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -9,18 +9,20 @@ const apiRouter = require('express').Router();
 const db = require('../../config/db');
 const axios = require('axios');
 const reputationRouter = require('./reputation')
+const jwt = require('express-jwt')
 const sessionRouter = require('./session')
+const authMiddleware = jwt({secret: 'notsosecret'})
 
 const MAX_RESULTS = 100;
 
-apiRouter.get('/availabilities', (req, res, next) => {
+apiRouter.get('/availabilities', authMiddleware, (req, res, next) => {
   db.getAvailabilities(MAX_RESULTS, (err, results) => {
     if (err) next(err);
     res.send(results);
   });
 });
 
-apiRouter.use('/reputation', reputationRouter)
+apiRouter.use('/reputation', authMiddleware, reputationRouter)
 apiRouter.use('/session', sessionRouter)
 
 module.exports = apiRouter;
