1554cec34e55f74489606c1e6d4867ac70837118
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2016-12-24 01:50:49 +0530

    filter appointments by account and patient

--- server/routes/api/appointment/index.js
+++ server/routes/api/appointment/index.js
@@ -5,22 +5,21 @@ const Appointment = require('../../../models/Appointment');
 
 const appointmentSchema = new Schema('appointments');
 
-// TODO: this should have default queries and limits
 appointmentsRouter.get('/', (req, res, next) => {
-  // activeaccount from req.user
   console.log('user', req.user)
-  // TODO: ensure that we only pull appointments for activeAccount
-  Appointment.run()
+  Appointment.filter({
+    accountId: req.user.data.activeAccountId
+  }).run()
     .then(appointments => res.send(normalize(appointments, arrayOf(appointmentSchema))))
     .catch(next);
 });
 
 appointmentsRouter.get('/:patientId', (req, res, next) => {
-  // todo: implement me
-  // const { patientId } = req.params;
-  // Appointment.get(patientId)
-  //   .then(patient => res.send(normalize(patient, appointmentSchema)))
-  //   .catch(next);
+  Appointment.filter({
+    patientId: req.params.patientId
+  }).run()
+    .then(appointments => res.send(normalize(appointments, arrayOf(appointmentSchema))))
+    .catch(next);
 });
 
 module.exports = appointmentsRouter;
