378e71713eb2cf80bbc6353523fbb6e011d40064
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2016-12-24 03:08:28 +0530

    modify seeds for relations

--- server/bin/seeds.js
+++ server/bin/seeds.js
@@ -1,7 +1,7 @@
 
 const bcrypt = require('bcrypt');
 const seedDatabase = require('../util/seedDatabase');
-const addRelations = seedDatabase.addRelationsToDatabase;
+const uuid = require('uuid').v4
 
 // For hashing passwords for User seeds
 // TODO: pull fromm global config, cause needs to be reused with deserialization
@@ -18,45 +18,67 @@ const saltRounds = 10;
  *
  */
 
+const accountId = uuid();
+const justinPatientId = uuid();
+
 const SEEDS = {
-  // Appointment: [
-  //   {
-  //     start: new Date(2016, 11, 2, 9, 30, 0, 0),
-  //     end: new Date(2016, 11, 2, 12, 30, 0, 0),
-  //     title: 'Crown Availability',
-  //   },
-  //   {
-  //     start: new Date(2016, 11, 2, 14, 30, 0, 0),
-  //     end: new Date(2016, 11, 2, 15, 30, 0, 0),
-  //     title: 'Cavity Availability',
-  //   },
-  //   {
-  //     start: new Date(2016, 11, 3, 9, 30, 0, 0),
-  //     end: new Date(2016, 11, 3, 13, 30, 0, 0),
-  //     title: 'Braces Availability',
-  //   },
-  // ],
+  Appointment: [
+    {
+      start: new Date(2016, 12, 26, 9, 30, 0, 0),
+      end: new Date(2016, 12, 26, 12, 30, 0, 0),
+      title: 'Crown Availability',
+      id: uuid(),
+      accountId: accountId,
+      patientId: justinPatientId,
+    },
+    {
+      start: new Date(2016, 12, 27, 14, 30, 0, 0),
+      end: new Date(2016, 12, 27, 15, 30, 0, 0),
+      title: 'Cavity Availability',
+      id: uuid(),
+      accountId: accountId,
+      patientId: justinPatientId,
+    },
+    {
+      start: new Date(2016, 12, 28, 9, 30, 0, 0),
+      end: new Date(2016, 12, 28, 13, 30, 0, 0),
+      title: 'Braces Availability',
+      id: uuid(),
+      accountId: accountId,
+      patientId: justinPatientId,
+    },
+  ],
   
   User: [
     {
       username: 'lonny@carecru.com',
       password: bcrypt.hashSync('lonny', saltRounds),
+      id: uuid(),
+      activeAccountId: accountId,
     },
     {
       username: 'mark@carecru.com',
       password: bcrypt.hashSync('mark', saltRounds),
+      id: uuid(),
+      activeAccountId: accountId,
     },
     {
       username: 'justin@carecru.com',
       password: bcrypt.hashSync('justin', saltRounds),
+      id: uuid(),
+      activeAccountId: accountId,
     },
     {
       username: 'ashmeet@carecru.com',
       password: bcrypt.hashSync('ashmeet', saltRounds),
+      id: uuid(),
+      activeAccountId: accountId,
     },
     {
       username: 'sergey@carecru.com',
       password: bcrypt.hashSync('sergey', saltRounds),
+      id: uuid(),
+      activeAccountId: accountId,
     },
   ],
   
@@ -65,16 +87,19 @@ const SEEDS = {
       firstName: 'Justin',
       lastName: 'Sharp',
       phoneNumber: '+17808508886',
+      id: justinPatientId,
     },
     {
       firstName: 'Sergey',
       lastName: 'Skovorodnikov',
       phoneNumber: '+17782422626',
+      id: uuid(),
     },
     {
       firstName: 'Mark',
       lastName: 'Joseph',
       phoneNumber: '+17788654451',
+      id: uuid(),
     },
   ],
 
@@ -83,6 +108,7 @@ const SEEDS = {
       name: 'Beckett Dental',
       vendastaId: 'UNIQUE_CUSTOMER_IDENTIFIER',
       smsPhoneNumber: '+17786558613',
+      id: accountId,
     }
   ]
 };
@@ -90,11 +116,10 @@ const SEEDS = {
 seedDatabase(SEEDS)
   .then(() => {
     console.log('Successfully executed bin/seeds.')
-    addRelations()
-    // process.exit();
+    process.exit();
   })
   .catch((err) => {
     console.error('Unsuccessfully executed bin/seeds.')
     console.error(err);
-    // process.exit(1);
+    process.exit(1);
   });
