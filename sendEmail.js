7226b6e665b454d6f050538257a4afd9ae4d4989
Justin Sharp <justin.d.sharp@gmail.com>
2017-01-12 23:49:07 -0800

    showed how to use MERGE_VARS

--- sendEmail.js
+++ sendEmail.js
@@ -1,8 +1,18 @@
 
 const mail = require('./server/lib/mail');
 
+const protocol = 'https';
+const host = 'localhost:5000';
+const token = 'token-test';
+
 mail.sendConfirmationReminder({
   toEmail: 'justin@carecru.com',
+  mergeVars: [
+    {
+      name: 'CONFIRMATION_URL',
+      content: `${protocol}://${host}/confirmation/${token}`,
+    },
+  ]
 }).then(() => {
   console.log('Email Sent');
   process.exit();
