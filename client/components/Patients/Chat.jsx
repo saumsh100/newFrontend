81c2a98e2dff49f7d3dbccb854a4a2e8a09d1c81
michaelost <mykhailo.ostroviy@keenethics.com>
2017-01-16 18:45:07 +0200

    display messages

--- client/components/Patients/Chat.jsx
+++ client/components/Patients/Chat.jsx
@@ -2,6 +2,7 @@
 import React, { PropTypes, Component } from 'react';
 import { Card } from '../library';
 import SendMessageInput from './SendMessageInput';
+import moment from 'moment';
 
 class Chat extends Component {
   constructor(props) {
@@ -28,45 +29,38 @@ class Chat extends Component {
     console.log(textMessages.get('models').size);
     
     if (patient === null) return <div>Loading...</div>;
-    
+
     return (
-      <Card style={{ height: '400px' }}>
-        <div style={{ padding: '0px', position: 'relative', height: '100%' }}>
-
-
-
-
-          <div style={{ overflowY: 'scroll', height: '100%' }}>
-
-
-
-            <ul className="dialogs__messages">
-                <li className="messages">
-                    <img className="messages__photo" src="./img/people.png" alt="" />
-                    <div className="messages__wrapper">
-                        <div className="messages__header">
-                            <div className="messages__name">Issac Bryne, 47</div>
-                            <div className="messages__date">9:00am</div>
-                        </div>
-                        <div className="messages__body">
-                            <div className="messages__unread">
-                                <span> 2 </span>
-                            </div>
-                            <div className="messages__text">Lourem ipsum eccodi san deserif
-                                codi san deserif. Ipsum eccoesede
-                                desdjrdccodi s Lourem ipsum...
-                            </div>
-                        </div>
-                    </div>
-                </li>
-            </ul>
-
-
-            {textMessages.get('models').map(m => <div>{m.body}</div> )}
+        <Card style={{ height: '400px' }}>
+          <div style={{ padding: '0px', position: 'relative', height: '100%' }}>
+            <div style={{ overflowY: 'scroll', height: '100%' }}>
+              <ul className="dialogs__messages" >
+                {textMessages.get('models').map(m =>
+                    <li className="messages">
+                      <img className="messages__photo" src="./img/people.png" alt="" />
+                      <div className="messages__wrapper">
+                          <div className="messages__header">
+                              <div className="messages__name"></div>
+                              <div className="messages__date">
+                                {moment(m.createdAt).format('MMMM Do YYYY, h:mm:ss a')}
+                              </div>
+                          </div>
+                          <div className="messages__body">
+                              <div className="messages__unread">
+                                  <span></span>
+                              </div>
+                              <div className="messages__text">
+                                {m.body}
+                              </div>
+                          </div>
+                      </div>
+                  </li>
+                )}
+              </ul>
+            </div>
+            <SendMessageInput onSend={this.sendMessage} />
           </div>
-          <SendMessageInput onSend={this.sendMessage} />
-        </div>
-      </Card>
+        </Card>
     );
   }
 }
