cbec93bc2562ee97f4bad25916cd40eb81912f0a
sasha.bashliy <sashkacrab@gmail.com>
2017-01-24 17:39:57 +0200

    possibly working remind job

--- server/bin/cron.js
+++ server/bin/cron.js
@@ -2,10 +2,13 @@
 const cron = require('node-cron');
 const remindersQueue = require('../config/queue/remindersQueue');
 
-cron.schedule('*/20 * * * * *', () => {
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const cronPattern = NODE_ENV === 'production' ? '0 */30  * * * *' : '0 */1 * * * *';
+
+cron.schedule(cronPattern, () => {
   // Tell the reminders process to compute the reminders that have to be sent
-  const testData = Date.now();
-  remindersQueue.addJob({ testData })
+  const job = remindersQueue.createJob({ retryMax: 2, mili24hours: 86400000 });
+  remindersQueue.addJob(job)
     .then(savedJobs => console.log('savedJobs', savedJobs))
     .catch(err => console.error(err));
 });
