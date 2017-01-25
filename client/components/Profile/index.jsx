cde7efd3a1ee055250a50ca972322c4dfd3fc62a
Justin Sharp <sharp@Justins-MBP.hitronhub.home>
2017-01-25 13:36:38 -0800

    pushing up an example of initialValues for michael

--- client/components/Profile/index.jsx
+++ client/components/Profile/index.jsx
@@ -4,12 +4,15 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Card } from '../library';
 import ChangePasswordForm from './ChangePasswordForm';
+import TestForm from '../demo/TestForm';
 import { changePassword } from '../../thunks/user';
 
 function Profile(props) {
   return (
     <Card>
       <ChangePasswordForm onSubmit={props.changePassword} />
+      <br/><br/>
+      <TestForm patient={{ firstName: 'Justin', middleName: 'Daniel', lastName: 'Sharp' }} />
     </Card>
   );
 }
