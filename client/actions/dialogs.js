ad2df34e1ee21c704630ca2d0bd5fbc298c33830
michaelost <mikhaylo.ostroviy@gmail.com>
2017-01-30 17:55:18 +0200

    implement dialogs searching

--- client/actions/dialogs.js
+++ client/actions/dialogs.js
@@ -2,6 +2,8 @@
 import { createAction } from 'redux-actions';
 import {
 	SET_CURRENT_DIALOG,
+	SET_DIALOGS_FILTER
 } from '../constants';
 
 export const setCurrentDialogAction = createAction(SET_CURRENT_DIALOG);
+export const setDialogsFilterAction = createAction(SET_DIALOGS_FILTER);
--- client/thunks/dialogs.js
+++ client/thunks/dialogs.js
@@ -1,10 +1,17 @@
 import {
 	setCurrentDialogAction,
-	sendMessageOnClientAction
+	sendMessageOnClientAction,
+	setDialogsFilterAction,
 } from '../actions/dialogs';
 
 export function setCurrentDialog(currentDialogId) {
   return function (dispatch, getState) {
     dispatch(setCurrentDialogAction({ currentDialogId }));
+  };
+}
+
+export function setDialogsFilter(username) {
+  return function (dispatch, getState) {
+    dispatch(setDialogsFilterAction({ username }));
   };
 }
