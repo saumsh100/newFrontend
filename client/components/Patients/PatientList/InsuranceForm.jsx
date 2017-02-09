4dc47157af06e45b55ba1211f52d0c7e9998318f
michaelost <mikhaylo.ostroviy@gmail.com>
2017-02-09 12:40:08 +0200

    change editing insurance information flow

---
+++ client/components/Patients/PatientList/InsuranceForm.jsx
@@ -0,0 +1,137 @@
+import React, { Component, PropTypes } from 'react';
+import moment from 'moment';
+import styles from './main.scss';
+import { Button, Form, Field } from '../../library';
+import { Field as RField } from 'redux-form';
+import { destroy } from 'redux-form';
+class InsuranceForm extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClickSave = this.handleClickSave.bind(this);
+    this.state = { formChanged: false };
+  }
+
+  componentWillReceiveProps(nextprops) {
+    if (nextprops.patient.id !== this.props.patient.id) {
+      const { patient } = this.props;
+      const dialogId = `insurance`;
+      store.dispatch(destroy(dialogId));
+    } else {
+      const { patient, form, tabTitle } = this.props;
+      if (!patient) return;
+      let formChanged = false;
+      const currentPatientFormFields = nextprops.form.insurance.values;
+
+      const { sin, carrier, contract, memberId, insurance } = nextprops.patient.insurance;
+      const currentPatientfields = { sin, carrier, contract, memberId, insurance }; 
+      const currentPatientKeys = Object.keys(currentPatientfields);
+      currentPatientKeys.forEach(p => {
+        if (currentPatientfields[p] !== currentPatientFormFields[p]) {
+          formChanged = true;
+          return;
+        }
+      })
+      if (formChanged !== this.state.formChanged) {
+        this.setState({ formChanged });
+      }
+    }
+  }
+
+
+  handleClickSave(e) {
+    e.preventDefault();
+    const {
+      patient,
+      form,
+      tabTitle,
+      changePatientInfo,
+    } = this.props;
+    const formData = tabTitle;
+    const values = form[formData].values;
+    values.id = patient.id;
+    values.title = tabTitle;
+    changePatientInfo(values);
+  }
+
+  render() {
+    const { tabTitle, patient } = this.props;
+    const dialogId = tabTitle;
+    const initialValues = patient.insurance || {};
+    const saveBtnClass = `${styles.edit_personal__btn} ${this.state.formChanged ? styles.edit_personal__btn_enabled : ''}`
+
+    return (
+      <div className={styles.right__personal} onClick={this.handleClick}>
+        <div className={styles.edit_insurance}>
+          <Form form={dialogId}
+            initialValues={initialValues}>
+            <div className={styles.edit_insurance__header}>
+              <div className={`${styles.edit_insurance__company} ${styles.edit_insurance__table}`}>
+                <div className={styles.icon}>
+                  <i className="fa fa-medkit" />
+                </div>
+                <Field
+                  className={styles.edit_insurance__input}
+                  type="text"
+                  name="insurance"
+                  placeholder="Insurance #"
+                  min
+                />
+              </div>
+              <div className={`${styles.edit_insurance__id} ${styles.edit_insurance__table}`}>
+                <Field
+                  className={styles.edit_insurance__input}
+                  type="text"
+                  name="memberId"
+                  placeholder="Member ID #"
+                  min
+                />
+
+              </div>
+              <div className={`${styles.edit_insurance__contact} ${styles.edit_insurance__table}`}>
+                <Field
+                  className={styles.edit_insurance__input}
+                  type="text"
+                  name="contract"
+                  placeholder="Contract #"
+                  min
+                />
+
+              </div>
+              <div className={`${styles.edit_insurance__carrier} ${styles.edit_insurance__table}`}>
+                <Field
+                  className={styles.edit_insurance__input}
+                  name="carrier"
+                  placeholder="Carrier #"
+                  min
+                />
+              </div>
+            </div>
+            <div className={styles.edit_insurance__footer}>
+              <div className={`${styles.edit_insurance__carrier} ${styles.edit_insurance__table}`}>
+                <div className={styles.icon}>
+                  <i className="fa fa-id-card-o" />
+                </div>
+                <Field
+                  className={styles.edit_insurance__input}
+                  name="sin"
+                  placeholder="SIN #"
+                  min
+                />
+              </div>
+            </div>
+            <input
+              onClick={this.handleClickSave}
+              className={saveBtnClass}
+              type="submit"
+              value="Save"
+              disabled={!this.state.formChanged ? true : false }
+            />
+          </Form>
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default InsuranceForm;
--- client/components/Patients/PatientList/PersonalData.jsx
+++ client/components/Patients/PatientList/PersonalData.jsx
@@ -5,19 +5,17 @@ import { Button, Form, Field } from '../../library';
 import { Field as RField } from 'redux-form'
 import PersonalForm from './PersonalForm';
 
-class PersonalData extends Component {
-  render() {
-    const { patient, currentPatientState, tabTitle } = this.props;
-    if (!patient) {
-      return <div className={styles.loading}>Loading...</div>;
-    }
-    return <PersonalForm 
-      patient={patient}
-      tabTitle={this.props.tabTitle}
-      form={this.props.form}
-      changePatientInfo={this.props.changePatientInfo}
-      />
+export default function PersonalData(props) {
+  const { patient, currentPatientState, tabTitle } = props;
+  if (!patient) {
+    return <div className={styles.loading}>Loading...</div>;
   }
+  return (
+    <PersonalForm 
+      patient={patient}
+      tabTitle={props.tabTitle}
+      form={props.form}
+      changePatientInfo={props.changePatientInfo}
+    />
+  ); 
 }
-
-export default PersonalData;
