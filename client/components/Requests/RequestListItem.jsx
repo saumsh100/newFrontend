8b41f777f0a0de50ec97ee2d92f6a7c3d7b5d8aa
jatd <dhillon.jatinder@gmail.com>
2017-02-14 11:28:01 -0800

    completing hover component.

--- client/components/Requests/RequestListItem.jsx
+++ client/components/Requests/RequestListItem.jsx
@@ -1,43 +1,59 @@
-
 import React, { Component, PropTypes } from 'react';
-import moment from 'moment';
 import { ListItem } from '../library';
 import MonthDay from './MonthDay';
 import RequestData from './RequestData';
 import styles from './styles.scss';
 import AppointmentShowData from '../Appointment/AppointmentShowData';
 
+class RequestListItem extends Component {
 
-export default function RequestListItem({ request, patient, service, practitioner,}) {
+  constructor(props){
+    super(props)
+    this.state = {
+      showResults: false,
+    }
+    this.showAppointment = this.showAppointment.bind(this);
+    this.hideAppointment = this.hideAppointment.bind(this);
 
-  const data = {
-    time: request.getFormattedTime(),
-    nameAge: patient.getFullName().concat(', ', request.getAge(patient.birthday)),
-    email: patient.email,
-    service: service.name,
-    phoneNumber: patient.phoneNumber,
-    insurance: patient.getInsurance().insurance,
-    comment: request.comment,
-    month: request.getMonth(),
-    day: request.getDay(),
-  };
+  }
 
-  const showResults = true;
+  showAppointment() {
+    this.setState({showResults: true});
+  }
 
-  return (
-    <div>
-      <ListItem className={styles.requestListItem}>
-        <MonthDay month={data.month} day={data.day} />
-        <RequestData data={data} />
-      </ListItem>
-      {showResults ? <AppointmentShowData data={data} />: null }
-    </div>
-  );
+  hideAppointment(){
+    this.setState({ showResults: false });
+  }
+
+  render() {
+    const {request, patient, service } = this.props;
+
+    const data = {
+      time: request.getFormattedTime(),
+      nameAge: patient.getFullName().concat(', ', request.getAge(patient.birthday)),
+      email: patient.email,
+      service: service.name,
+      phoneNumber: patient.phoneNumber,
+      insurance: patient.getInsurance().insurance,
+      comment: request.comment,
+      month: request.getMonth(),
+      day: request.getDay(),
+    };
+    return (
+      <div>
+        <ListItem onMouseOver={this.showAppointment} onMouseOut={this.hideAppointment} className={styles.requestListItem}>
+          <MonthDay month={data.month} day={data.day} />
+          <RequestData data={data}/>
+        </ListItem>
+        {this.state.showResults ? <AppointmentShowData data={data} /> : null }
+      </div>
+    );
+  }
 }
-
-
 
 RequestListItem.propTypes = {
   patient: PropTypes.object.isRequired,
   request: PropTypes.object.isRequired,
 };
+
+export default RequestListItem;
