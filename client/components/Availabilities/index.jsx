0f7d56b6d6269cac3c905a584318d1e069a8c3f2
michaelost <mikhaylo.ostroviy@gmail.com>
2017-02-19 19:28:45 +0200

    minor fix

--- client/components/Availabilities/index.jsx
+++ client/components/Availabilities/index.jsx
@@ -107,6 +107,130 @@ class Availabilities extends React.Component {
     console.log('Click handled')
   }
 
+  renderFirstStep(practitionerId, services, availabilities, practitioners) {
+    return (
+      <div className={styles.appointment}>
+        <div className={styles.appointment__wrapper}>
+          <div className={styles.appointment__sidebar}>
+          </div>
+          <div className={styles.appointment__main}>
+            <div className={styles.appointment__header}>
+              <div className={styles.appointment__header_title}>
+                BOOK APPOINTMENT
+              </div>
+            </div>
+            <div className={styles.appointment__body}>
+              <div className={styles.appointment__body_header}>
+                <div className={styles.appointment__select_title}>Practitioner</div>
+                <div className={styles.appointment__select_wrapper}>
+                  <select
+                    className={styles.appointment__select_item}
+                    value={practitionerId}
+                    onChange={this.onDoctorChange}
+                    ref={(doctor) => {
+                      this.doctor = doctor;
+                    }}>
+                    <option value="" selected>No Preference</option>
+                    {practitioners.map(p =>
+                      <option value={p.id} key={p.id}>{p.getFullName()}</option>
+                    )}
+                  </select>
+                  <select
+                    className={styles.appointment__select_item}
+                    onChange={this.onServiceChange}
+                    ref={(service) => {
+                      this.service = service;
+                    }}>
+                    <option value="" selected>Service</option>
+                    {services.filter(s =>
+                      includes(s.practitioners, this.state.practitionerId)
+                    ).map(s =>
+                      <option value={s.id} key={s.id}>{s.name}</option>
+                    )}
+                  </select></div>
+                <div className={styles.appointment__daypicker}>
+                  <div className={styles.appointment__daypicker_title}>Appointment scheduler</div>
+                  <div onClick={this.openModal}
+                       className={styles.appointment__daypicker_icon}>
+                    <div className={styles.appointment__daypicker_date}>
+                      {moment(this.state.selectedStartDay).format('DD MM YYYY')}
+                    </div>
+                    <i className="fa fa-calendar"/>
+                  </div>
+                  {this.state.modalIsOpen ?
+                    (
+                      <div onClick={this.openModal} className={styles.appointment__daypicker_modal}>
+                        <DayPicker
+                          className={styles.appointment__daypicker_select}
+                          onDayClick={ this.handleDayClick }
+                          selectedDays={ this.isDaySelected }/>
+                      </div>
+                    ) : null
+                  }
+                </div>
+              </div>
+              <div className={styles.appointment__table}>
+                <button className={styles.appointment__table_btn} onClick={this.sixDaysBack}>
+                  <i className="fa fa-arrow-circle-o-left"/>
+                </button>
+                <div className={styles.appointment__table_elements}>
+                  {availabilities.map(av => {
+                    return (
+                      <ul className={styles.appointment__list} key={av.date}>
+                        <div className={styles.appointment__list_header}>
+                          <div className={styles.list__header_day}>
+                            {moment(av.date).format('ddd')}
+                          </div>
+                          <div className={styles.list__header_number}>
+                            {moment(av.date).format('DD/MM/YYYY')}
+                          </div>
+                        </div>
+                        {av.availabilities.map(slot =>
+                          <li
+                            className={`${styles.appointment__list_item} ${slot.isBusy ? styles.appointment__list_active : ''}`}
+                            key={slot.startsAt}>
+                            {moment(slot.startsAt).format('HH:mm A')}
+                          </li>)
+                        }
+                      </ul>);
+                  })}
+                </div>
+                <button className={styles.appointment__table_btn} onClick={this.sixDaysForward}>
+                  <i className="fa fa-arrow-circle-o-right"/>
+                </button>
+              </div>
+              <div className={styles.appointment__footer}>
+                <div className={styles.appointment__footer_title}>
+                  would you like to be informed if an earlier time becomes available?
+                </div>
+                <form className={styles.appointment__footer_confirm}>
+                  <div className={styles.appointment__footer_select}>
+                    <input type="radio" name="answer" id="yes" value="yes"/>
+                    <label htmlFor="yes">Yes</label>
+                    <input type="radio" name="answer" id="no" value="no"/>
+                    <label htmlFor="no">No</label>
+                  </div>
+                  <input onClick={this.saveAndContinue} className={styles.appointment__footer_btn} type="submit"
+                         value="Continue"/>
+                </form>
+                <div className={styles.appointment__footer_pagination}>
+                  <ul>
+                    <li>
+                      <div>1</div>
+                    </li>
+                    <li>
+                      <div>2</div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   render() {
     const {
       services,
@@ -114,129 +238,14 @@ class Availabilities extends React.Component {
       availabilities,
       practitionerId,
     } = this.props;
+
+    const serviceId = services[0] && services[0].id;
+    const prId =  practitioners[0] && practitioners.id;
+    const defaultValues = { practitionerId: prId, serviceId };
+
     switch (this.state.step) {
       case 1:
-        return (
-          <div className={styles.appointment}>
-            <div className={styles.appointment__wrapper}>
-              <div className={styles.appointment__sidebar}>
-              </div>
-              <div className={styles.appointment__main}>
-                <div className={styles.appointment__header}>
-                  <div className={styles.appointment__header_title}>
-                    BOOK APPOINTMENT
-                  </div>
-                </div>
-                <div className={styles.appointment__body}>
-                  <div className={styles.appointment__body_header}>
-                    <div className={styles.appointment__select_title}>Practitioner</div>
-                    <div className={styles.appointment__select_wrapper}>
-                      <select
-                        className={styles.appointment__select_item}
-                        value={practitionerId}
-                        onChange={this.onDoctorChange}
-                        ref={(doctor) => {
-                          this.doctor = doctor;
-                        }}>
-                        <option value="" selected>No Preference</option>
-                        {practitioners.map(p =>
-                          <option value={p.id} key={p.id}>{p.getFullName()}</option>
-                        )}
-                      </select>
-                      <select
-                        className={styles.appointment__select_item}
-                        onChange={this.onServiceChange}
-                        ref={(service) => {
-                          this.service = service;
-                        }}>
-                        <option value="" selected>Service</option>
-                        {services.filter(s =>
-                          includes(s.practitioners, this.state.practitionerId)
-                        ).map(s =>
-                          <option value={s.id} key={s.id}>{s.name}</option>
-                        )}
-                      </select></div>
-                    <div className={styles.appointment__daypicker}>
-                      <div className={styles.appointment__daypicker_title}>Appointment scheduler</div>
-                      <div onClick={this.openModal}
-                           className={styles.appointment__daypicker_icon}>
-                        <div className={styles.appointment__daypicker_date}>
-                          {moment(this.state.selectedStartDay).format('DD MM YYYY')}
-                        </div>
-                        <i className="fa fa-calendar"/>
-                      </div>
-                      {this.state.modalIsOpen ?
-                        (
-                          <div onClick={this.openModal} className={styles.appointment__daypicker_modal}>
-                            <DayPicker
-                              className={styles.appointment__daypicker_select}
-                              onDayClick={ this.handleDayClick }
-                              selectedDays={ this.isDaySelected }/>
-                          </div>
-                        ) : null
-                      }
-                    </div>
-                  </div>
-                  <div className={styles.appointment__table}>
-                    <button className={styles.appointment__table_btn} onClick={this.sixDaysBack}>
-                      <i className="fa fa-arrow-circle-o-left"/>
-                    </button>
-                    <div className={styles.appointment__table_elements}>
-                      {availabilities.map(av => {
-                        return (
-                          <ul className={styles.appointment__list} key={av.date}>
-                            <div className={styles.appointment__list_header}>
-                              <div className={styles.list__header_day}>
-                                {moment(av.date).format('ddd')}
-                              </div>
-                              <div className={styles.list__header_number}>
-                                {moment(av.date).format('DD/MM/YYYY')}
-                              </div>
-                            </div>
-                            {av.availabilities.map(slot =>
-                              <li
-                                className={`${styles.appointment__list_item} ${slot.isBusy ? styles.appointment__list_active : ''}`}
-                                key={slot.startsAt}>
-                                {moment(slot.startsAt).format('HH:mm A')}
-                              </li>)
-                            }
-                          </ul>);
-                      })}
-                    </div>
-                    <button className={styles.appointment__table_btn} onClick={this.sixDaysForward}>
-                      <i className="fa fa-arrow-circle-o-right"/>
-                    </button>
-                  </div>
-                  <div className={styles.appointment__footer}>
-                    <div className={styles.appointment__footer_title}>
-                      would you like to be informed if an earlier time becomes available?
-                    </div>
-                    <form className={styles.appointment__footer_confirm}>
-                      <div className={styles.appointment__footer_select}>
-                        <input type="radio" name="answer" id="yes" value="yes"/>
-                        <label htmlFor="yes">Yes</label>
-                        <input type="radio" name="answer" id="no" value="no"/>
-                        <label htmlFor="no">No</label>
-                      </div>
-                      <input onClick={this.saveAndContinue} className={styles.appointment__footer_btn} type="submit"
-                             value="Continue"/>
-                    </form>
-                    <div className={styles.appointment__footer_pagination}>
-                      <ul>
-                        <li>
-                          <div>1</div>
-                        </li>
-                        <li>
-                          <div>2</div>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        );
+        return this.renderFirstStep(practitionerId, services, availabilities, practitioners)
       case 2:
         return <SignUp />
     }
