cd1c4a7723b966f1a5ccfe66e5c6215ee0261001
sasha.bashliy <sashkacrab@gmail.com>
2017-02-21 20:17:03 +0200

    commit

--- server/routes/my/index.js
+++ server/routes/my/index.js
@@ -6,7 +6,15 @@ const practitionersRouter = require('../api/practitioners');
 apiRouter.use('/practitioners', practitionersRouter);
 
 myRouter.get('/', (req, res, next) => {
+  console.log('loggg');
   return res.render('patient');
+});
+
+myRouter.get('/widgets/:accountId', (req, res, next) => {
+  console.log('log');
+  return res.render('widget', {
+    accountId: req.params.accountId,
+  });
 });
 
 myRouter.use(apiRouter);
