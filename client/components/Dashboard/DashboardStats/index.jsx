144f5cd1709c0d9d5255d421a2117eafde544002
Delphit <you@example.com>
2017-03-16 21:46:42 +0200

    Fix flex layout in DashboardStats

--- client/components/Dashboard/DashboardStats/index.jsx
+++ client/components/Dashboard/DashboardStats/index.jsx
@@ -4,28 +4,28 @@ import { Row, Col, IconCard  } from '../../library'
 import styles from './styles.scss';
 
 export default function DashboardStats(props) {
-  const {
-    className,
-    count,
-    title,
-    icon,
-    size,
-  } = props;
+    const {
+        className,
+        count,
+        title,
+        icon,
+        size,
+    } = props;
 
-  return (
-    <Row className={styles.dashboardStats}>
-      <Col xs={12} sm={3}>
-        <IconCard className={styles.primaryColor} count="12" title="Appointment Booked" icon="calendar" size={10} />
-      </Col>
-      <Col xs={12} sm={3}>
-        <IconCard className={styles.primaryBlue} count="64" title="Appointment Booked" icon="user" size={10} />
-      </Col>
-      <Col xs={12} sm={3}>
-        <IconCard className={styles.primaryGreen} count="16" title="Appointment Booked" icon="bullhorn" size={10} />
-      </Col>
-      <Col xs={12} sm={3}>
-        <IconCard className={styles.primaryYellow} count="23" title="Appointment Booked" icon="star" size={10} />
-      </Col>
-    </Row>
-  );
+    return (
+        <Row className={styles.dashboardStats}>
+            <Col className={styles.dashboardStats__item} xs={12} sm={6} md={3}>
+                <IconCard className={styles.primaryColor} count="12" title="Appointment Booked" icon="calendar" size={10} />
+            </Col>
+            <Col className={styles.dashboardStats__item} xs={12} sm={6} md={3}>
+                <IconCard className={styles.primaryBlue} count="64" title="Appointment Booked" icon="user" size={10} />
+            </Col>
+            <Col className={styles.dashboardStats__item} xs={12} sm={6} md={3}>
+                <IconCard className={styles.primaryGreen} count="16" title="Appointment Booked" icon="bullhorn" size={10} />
+            </Col>
+            <Col className={styles.dashboardStats__item} xs={12} sm={6} md={3}>
+                <IconCard className={styles.primaryYellow} count="23" title="Appointment Booked" icon="star" size={10} />
+            </Col>
+        </Row>
+    );
 }
