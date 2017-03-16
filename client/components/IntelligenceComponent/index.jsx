bfbae419501e0d8ea01aa70d704e0d759ea2da27
michaelost <mikhaylo.ostroviy@gmail.com>
2017-03-16 14:11:06 +0200

    make flexGrid

--- client/components/IntelligenceComponent/index.jsx
+++ client/components/IntelligenceComponent/index.jsx
@@ -1,44 +1,52 @@
 import React, { PropTypes, Component } from 'react';
-import { Card, CardHeader, Col, Grid, Row, FlexGrid } from '../library';
+import { Card, CardHeader, Col, Grid, Row } from '../library';
 import ChartGrid from '../library/ChartGrid'
+import { FlexGrid, Stats } from '../library/FlexGrid';
 
 class IntelligenceComponent extends Component {
   render() {
     return (
       <div>
 	      <Grid>
-			      <Row>
-	      			<Col xs={12} md={6} >
-			        	<FlexGrid 
-			        		items={[
-			        			{count: 106, details: 'via phone', icon: 'phone', first: true},
-			        			{count: 206, details: 'via ...', icon: 'phone'},
-			        			{count: 306, details: 'viadsaa ...', icon: 'phone'},
-			        		]} 
-			        		title={"Appoinment Types"}
-			        	/>
-	      			</Col>
-	      			<Col xs={12} md={6} >
-			        	<FlexGrid 
-			        		items={[
-			        			{count: 206, details: 'via facebook', icon: 'phone', first: true},
-			        			{count: 406, details: '...phone', icon: 'phone'}
-			        		]}
-			        		title={"Appoinment Types"}
-			        	/>
-			      	</Col>
+		      <Row>
+      			<Col xs={12} md={6} >
+		        	<FlexGrid title="Appoinment Types" > 
+        				<Stats
+        					count={106}
+        					details="via phone"
+        					icon="phone"
+        					border
+        				/>
+        				<Stats
+        					count={106}
+        					details="via phone"
+        					icon="phone"
+        					border
+        				/>
+		        	</FlexGrid>
 
-	      			<Col xs={12} md={6} >
-			        	<ChartGrid
-			        		title="Online reputation"
-			        		stars={"4/5"}
-			        		text={"start"}
-			        		positive={12}
-			        		negative={3}
-			        		icon="star"
-			        	/>
-			      	</Col>
-			      </Row>
+		        	<FlexGrid title="Appoinment Types" > 
+        				<Stats
+        					count={106}
+        					details="via phone"
+        					icon="phone"
+        					border
+        				/>
+        				<Stats
+        					count={106}
+        					details="via phone"
+        					icon="phone"
+        					border
+        				/>
+        				<Stats
+        					count={306}
+        					details="phone"
+        					icon="phone"
+        					border
+        				/>
+		        	</FlexGrid>
+		        </Col>
+		      </Row>
 	      </Grid>
       </div>
     );
--- client/components/library/FlexGrid/index.jsx
+++ client/components/library/FlexGrid/index.jsx
@@ -8,26 +8,24 @@ const FlexGrid = function (props) {
   const {
     items,
     title,
+    children,
   } = props;
-  const itemWidth = `${100 / items.length}%`;
-  const itemStyle = { width: itemWidth };
-  const gridStyle = { width: '100%' }; 
+  const width = Math.floor( 12/ children.length);
+  const gridStyle = { width: '100%' };
+  console.log("item length")
+  console.log(children.length)
+  console.log("children")
+  console.log(children)
   return (
     <div style={gridStyle} className={styles.settingsFormsCol}>
       <div className={styles.sideByside} >
         <div className={styles.sideByside__title}>{title}</div>
         <div className={styles.sideByside__body}  >
-          {items.map(i => (
-          <div style={itemStyle} className={`${styles.sideByside__leftCol} ${i.first ? styles.sideByside__first : '' }`}  >
-            <div className={styles.sideByside__icon} >
-              <i className={`fa fa-${i.icon}`} />
-            </div>
-            <div className={styles.sideByside__count}>
-              <div>{i.count}</div>
-              <div className={styles.sideByside__small}>{i.details}</div>
-            </div>
-          </div>
-          ))}
+        {React.Children.map(children, child => (
+          <Col xs={width}>
+            {React.cloneElement(child)}
+          </Col>
+        ))}
         </div>
       </div>
     </div>
@@ -36,8 +34,32 @@ const FlexGrid = function (props) {
 }
 
 FlexGrid.propTypes = {
-  // children: PropTypes.object.isRequired,
+
 };
 
+const Stats = function (props) {
+  const { 
+    border,
+    icon,
+    count,
+    details,
+  } = props;
+  return (
+      <div className={`${styles.sideByside__leftCol} ${border ? styles.sideByside__first : '' }`}  >
+        <div className={styles.sideByside__icon} >
+          <i className={`fa fa-${icon}`} />
+        </div>
 
-export default FlexGrid;
+        <div className={styles.sideByside__count}>
+          <div>{count}</div>
+          <div className={styles.sideByside__small}>{details}</div>
+        </div>
+      </div>
+  );
+}
+
+Stats.propTypes = {
+  
+};
+
+export { FlexGrid, Stats };
