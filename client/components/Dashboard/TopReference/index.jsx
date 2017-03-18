a84e82726e162992cf3c2cb9c7f3a3c3ad6d934c
Delphit <you@example.com>
2017-03-18 14:44:48 +0200

    Complete markup TopReference

--- client/components/Dashboard/TopReference/index.jsx
+++ client/components/Dashboard/TopReference/index.jsx
@@ -2,92 +2,38 @@ import React, { Component } from 'react';
 import Item from './Item'
 import styles from './styles.scss';
 
-
 class TopReference extends Component {
-  render() {
-    const hardcodeData = [[{
-      img: "images/practitioner_1.png",
-      name: "Konner Beck",
-      age: "2",
-      cardTitle: "Top referrers",
-      number: 4,
-    },{
-      img: "images/practitioner_1.png",
-      name: "Issac Item",
-      age: "2",
-      cardTitle: "Top referrers",
-      number: 4,
-    },{
-      img: "images/practitioner_1.png",
-      name: "Konner Beck",
-      age: "2",
-      cardTitle: "Top referrers",
-      number: 4,
-    },{
-      img: "images/practitioner_1.png",
-      name: "Issac Item",
-      age: "2",
-      cardTitle: "Top referrers",
-      number: 4,
-    },],[{
-      img: "images/practitioner_1.png",
-      name: "Issac Item",
-      age: "2",
-      cardTitle: "Top referrers",
-      number: 4,
-    },{
-      img: "images/practitioner_1.png",
-      name: "Konner Beck",
-      age: "2",
-      cardTitle: "Top referrers",
-      number: 4,
-    },{
-      img: "images/practitioner_1.png",
-      name: "Issac Item",
-      age: "2",
-      cardTitle: "Top referrers",
-      number: 4,
-    },{
-      img: "images/practitioner_1.png",
-      name: "Konner Beck",
-      age: "2",
-      cardTitle: "Top referrers",
-      number: 4,
-    }],[{
-      img: "images/practitioner_1.png",
-      name: "Issac Item",
-      age: "2",
-      cardTitle: "Top referrers",
-      number: 4,
-    },{
-      img: "images/practitioner_1.png",
-      name: "Konner Beck",
-      age: "2",
-      cardTitle: "Top referrers",
-      number: 4,
-    },{
-      img: "images/practitioner_1.png",
-      name: "Syndee Hart",
-      age: "2",
-      cardTitle: "Top referrers",
-      number: 4,
-    },{
-      img: "images/practitioner_1.png",
-      name: "Syndee Hart",
-      age: "2",
-      cardTitle: "Top referrers",
-      number: 4,
-    }]];
-    return (
-      <div className={styles.patients}>
-        {hardcodeData.map(obj => {
-          return (
-            <Item className={styles.patients__item} data={obj}/>
-          )
-        })}
-      </div>
-    );
-  }
+    render() {
+        const hardcodeData = [{
+            img: "images/practitioner_1.png",
+            name: "Issac Item",
+            age: "2",
+            number: 4,
+        },{
+            img: "images/practitioner_1.png",
+            name: "Konner Beck",
+            age: "2",
+            number: 4,
+        },{
+            img: "images/practitioner_1.png",
+            name: "Issac Item",
+            age: "2",
+            number: 4,
+        },{
+            img: "images/practitioner_1.png",
+            name: "Konner Beck",
+            age: "2",
+            number: 4,
+        }];
+        return (
+            <div className={styles.patients}>
+              <Item className={styles.patients__item} cardTitle="Top Referrers" data={hardcodeData}/>
+              <Item className={styles.patients__item} cardTitle="Most Confirmed Referrals" data={hardcodeData}/>
+              <Item className={styles.patients__item} cardTitle="Most Appointments" data={hardcodeData}/>
+              <Item className={styles.patients__item} cardTitle="Most Business" data={hardcodeData}/>
+            </div>
+        );
+    }
 }
 
 
