0cf2b84f03cd0a60826362c01a995dc7ebccaea4
Sergey Skovorodnikov <sergey@carecru.com>
2017-03-23 11:42:41 -0700

    JWT auth for sockets
    Added JWT auth for sockets. Room joins based on accountId. Tested ok
    with dash nsp.

--- server/bin/server.js
+++ server/bin/server.js
@@ -2,11 +2,11 @@
 const http = require('http');
 const globals = require('../config/globals');
 const app = require('./app');
-const setupSocketIO = require('../sockets');
+const bindSockets = require('../sockets');
 
 const server = http.createServer(app);
 
-setupSocketIO(server);
+bindSockets(server);
 
 server.listen(globals.port, () => {
   console.log(`CareCru HTTP Server is running on port ${globals.port}`);
--- server/sockets/namespaces/dash/index.js
+++ server/sockets/namespaces/dash/index.js
@@ -1,32 +1,35 @@
+const socketIoJwt = require('socketio-jwt');
+const globals = require('../../../config/globals');
+
 
 module.exports = function setupDashNsp(io) {
   const dashNsp = io.of('/dash');
 
-  dashNsp.on('connection', (socket) => {
-    console.log('dashNsp connection');
-
-    // Message format is JSON
-    // roomName is accountId
-    // TODO get account id from JWT instead
-    socket.on('openRoom', (data) => {
-      const roomName = data.accountId;
-
+  dashNsp
+    .on('connection', socketIoJwt.authorize({
+      secret: globals.tokenSecret,
+      timeout: 15000,
+    }))
+    .on('authenticated', (socket) => {
+      const roomName = socket.decoded_token.activeAccountId;
+      console.log(`dashNsp connection. roomName=${roomName}`);
+      
+      // Message format is JSON
+      // roomName is accountId
+      socket.join(roomName);
       console.log('active rooms', io.sockets.adapter.rooms);
       console.log(`Opening room: ${roomName}`);
-
-      socket.join(roomName);
       socket.to(roomName).emit('newJoin', 'dash board joined');
+
+      socket.on('sendToRoom', (data) => {
+        console.log('active rooms', io.sockets.adapter.rooms);
+        console.log(`Sending to all in the room ${roomName}: data=${data.msg}`);
+
+        socket.to(roomName).emit('roomMessage', `${data.msg} ${roomName}`);
+      });
+    })
+    .on('unauthorized', (socket) => {
+      console.log('ERR unauth');
     });
-
-    // TODO Get account from JWT
-    socket.on('sendToRoom', (data) => {
-      const roomName = data.accountId;
-
-      console.log('active rooms', io.sockets.adapter.rooms);
-      console.log(`Sending to all in the room ${roomName}: data=${data.msg}`);
-
-      socket.to(roomName).emit('roomMessage', `hello all in room ${roomName}`);
-    });
-  });
   return dashNsp;
 };
