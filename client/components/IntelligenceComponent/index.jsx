bf2c7fc7216e9d81ae2502b18e42abf64058eb71
michaelost <mikhaylo.ostroviy@gmail.com>
2017-03-24 15:51:40 +0200

    fix bold font

--- client/components/IntelligenceComponent/index.jsx
+++ client/components/IntelligenceComponent/index.jsx
@@ -270,16 +270,16 @@ class IntelligenceComponent extends Component {
               <CardHeader className={styles.cardHeader} title="Male vs Famale" />
               <div className={classNames(styles.columnContainer, styles.justifyCenter, styles.height400)}>
                 <div className={classNames(styles.menContainer, styles.rowContainer, styles.spaceAround, styles.width100p)}>
-                  <div className={classNames(styles.menContainer__item, styles.columnContainer, styles.justifyCenter)} >
-                    <span className={classNames(styles.menContainer__item_text, styles.colorWoman, styles.font82, styles.fontWeight800, "fa fa-user-o")}></span>
-                    <span className={classNames(styles.menContainer__item_text, styles.colorWoman, styles.font82, styles.fontWeight800, styles.textAlignRight, styles.marginTop30)}>55%</span>
-                    <span className={classNames(styles.colorBlack, styles.font24)} >Famale</span>
-                  </div>
-
                   <div className={classNames(styles.menContainer__item, styles.columnContainer, styles.justifyCenter)}>
                     <span className={classNames(styles.menContainer__item_text, styles.colorMan, styles.font82, styles.fontWeight800, "fa fa-user-o")}></span>
                     <span className={classNames(styles.menContainer__item_text, styles.colorMan, styles.font82, styles.fontWeight800, styles.textAlignRight, styles.marginTop30)}>45%</span>
                     <span className={classNames(styles.colorBlack, styles.font24)} >Male</span>
+                  </div>
+
+                  <div className={classNames(styles.menContainer__item, styles.columnContainer, styles.justifyCenter)} >
+                    <span className={classNames(styles.menContainer__item_text, styles.colorWoman, styles.font82, styles.fontWeight800, "fa fa-user-o")}></span>
+                    <span className={classNames(styles.menContainer__item_text, styles.colorMan, styles.font82, styles.fontWeight800, styles.textAlignRight, styles.marginTop30)}>55%</span>
+                    <span className={classNames(styles.colorBlack, styles.font24)} >Famale</span>
                   </div>
                 </div>
               </div>
