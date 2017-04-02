ed334e802a622853af19cb580edb2c60cf2c1d6f
michaelost <mikhaylo.ostroviy@gmail.com>
2017-04-02 15:21:29 +0300

    add stars

--- client/components/ReputationComponent/Reviews/index.jsx
+++ client/components/ReputationComponent/Reviews/index.jsx
@@ -4,10 +4,24 @@ import Score from './Score';
 import Total from './Total';
 import colorMap from '../../library/util/colorMap';
 import styles from './styles.scss';
-
+import _ from 'lodash';
 
 class Reviews extends Component {
   render() {
+
+    const rating = {
+      5: 11,
+      4: 0,
+      3: 0,
+      2: 0,
+      1: 1,
+    }
+
+    const ratingStars = _.keys(rating).sort((a,b) => a > b);
+    const maxValue = _.max(_.values(rating));
+
+
+
     return (
       <Grid className={styles.reviews}>
         <Row>
@@ -42,21 +56,21 @@ class Reviews extends Component {
             </Card>
           </Col>
           
-          <Col className={styles.padding} xs={4} md={4}>
+          <Col className={styles.padding} xs={12} md={4} sm={6} lg={4} >
             <Card borderColor={colorMap.blue} className={styles.card}>
               <div className={styles.stats}>
                 <span className={styles.stats__count} >4.7</span>
                 <span className={styles.stats__title} >Average Rating</span>
                 <div className={styles.stats__rating}>
-                  <Star />
-                  <Star />
+                  <Star size={1.3} />
+                  <Star size={1.3} />
                 </div>
                 <span className={styles.stats__bottom}>Industry Average 4.1/5</span>
               </div>
             </Card>
           </Col>
 
-          <Col className={styles.padding} xs={4} md={4}>
+          <Col className={styles.padding} xs={12} md={4} sm={6} lg={4}>
             <Card borderColor={colorMap.blue} className={styles.card}>
               <div className={styles.stats}>
                 <span className={styles.stats__count} >12</span>
@@ -69,15 +83,35 @@ class Reviews extends Component {
             </Card>
           </Col>
           
-          <Col className={styles.padding} xs={4} md={4} >
+          <Col className={styles.padding} xs={12} md={4} sm={6} lg={4} >
             <Card borderColor={colorMap.blue} className={styles.card}>
               <div className={styles.stats}>
-                <span className={styles.stats__count} >4.7</span>
-                <span className={styles.stats__title} >Average Rating</span>
-                <div className={styles.stats__rating}>
-                  <Star />
-                  <Star />
-                </div>
+                  {ratingStars.map(r => {
+                    const rows = [];
+                    for(let i = 1; i <= r; i++ ) {
+                      rows.push(<Star size={1.3} />);
+                    }
+
+                    const width = rating[r] ? (Math.floor((rating[r] / maxValue) * 80)) : 5;
+                    const style = { width: `${width}%` };
+                    console.log("style");
+                    console.log(style);
+                    return (
+                      <div className={styles.content}>
+                        <div className={styles.content__stars}>
+                          {rows}
+                        </div>
+
+                        <div className={styles.content__bar}>
+                          <span
+                            style={style}
+                            className={styles.content__bar__percent}>
+                          </span>
+                          {rating[r]}
+                        </div>    
+                      </div>
+                    )
+                  })}
               </div>
             </Card>
           </Col>
