44af8012ab1a41a7332f414fb5239c45eccf9716
Justin Sharp <justin.d.sharp@gmail.com>
2017-04-05 14:28:00 -0600

    branched out after finishing up code

--- server/sockets/namespaces/dash/index.js
+++ server/sockets/namespaces/dash/index.js
@@ -1,89 +1,67 @@
 
 const socketIoJwt = require('socketio-jwt');
-const globals = require('../../../config/globals');
-const twilioClient = require('../../../config/twilio');
-const twilioConfig = require('../../../config/globals').twilio;
-const dashChangeFeeds = require('./dashChangeFeeds');
-const Appointment = require('../../../models/Appointment');
-const normalize = require('../../../routes/api/normalize');
+const { tokenSecret, namespaces } = require('../../../config/globals');
+// const twilioClient = require('../../../config/twilio');
+// const twilioConfig = require('../../../config/globals').twilio;
 
-const nsps = globals.namespaces;
-
+// JWT Connection options
 const socketIoOptions = {
-  secret: globals.tokenSecret,
-  // TODO change to lower value or remove
+  secret: tokenSecret,
+
+  // TODO: change to lower value or remove
   timeout: 15000,
 };
 
-module.exports = function setupDashNsp(io) {
-  io.of(nsps.dash)
-    .on('connection', socketIoJwt.authorize(socketIoOptions))
-    .on('authenticated', (socket) => {
-      const accountIdFromSocket = socket.decoded_token.activeAccountId;
-      const roomName = accountIdFromSocket;
+function setupDashboardNamespace(io) {
+  const dash = io.of(namespaces.dash);
 
-      // TODO JWT token verification
-      // console.log(`dashNsp connection. Joining client to roomName=${roomName}; connected to /dash=`, io.of('/dash').connected);
-      console.log(`dashNsp connection. Joining client to roomName=${roomName}; connected to /dash=`);
-      console.log('active rooms', JSON.stringify(io.sockets.adapter.rooms));
-      socket.join(roomName);
+  // Authorize socket connection
+  dash.on('connection', socketIoJwt.authorize(socketIoOptions));
 
-      io.of(nsps.dash).in(roomName).emit('newJoin', 'dash board joined'); // notify everyone that someone joined
+  // TODO: better handling/logging here, thrown Error here will break process
+  dash.on('unauthorized', (msg) => {
+    console.err('unauthorized socket: ', JSON.stringify(msg.data));
+    throw new Error(msg.data.type);
+  });
 
-      socket.on('sendMessage', (data) => {
-        const { patient, message } = data;
-        twilioClient.sendMessage({
-          to: patient.phoneNumber,
-          from: twilioConfig.number,
-          body: message,
-          // statusCallback: 'https://carecru.ngrok.io/twilio/status',
-        }).then((result) => {
-          // TODO: this is queued, and not delivered, so not techincally sent...
-          console.log(result);
-          // TextMessage.save({
-          //   id: result.sid,
-          //   to: result.to,
-          //   from: result.from,
-          //   body: result.body,
-          //   status: result.status,
-          // }).then(tm => console.log('SMS sent and saved', tm))
-          //   .catch(err => console.log(err));
-        }).catch((err) => {
-          console.log('Error sending SMS');
-          console.log(err);
-        });
-      });
+  // Join rooms on successful authentication
+  dash.on('authenticated', (socket) => {
+    const { activeAccountId } = socket.decoded_token;
 
-      dashChangeFeeds(io, accountIdFromSocket);
+    // TODO: JWT token verification
+    // console.log(`dashNsp connection. Joining client to roomName=${roomName}; connected to /dash=`, io.of('/dash').connected);
+    console.log(`dashNsp connection. Joining client to roomName=${activeAccountId}; connected to /dash=`);
+    console.log('active rooms', JSON.stringify(io.sockets.adapter.rooms));
+    socket.join(activeAccountId);
 
-      Appointment
-        .filter({ accountId: accountIdFromSocket, isSyncedWithPMS: true })
-        .changes({ squash: true })
-        .then((feed) => {
-          feed.each((error, doc) => {
-            if (error) throw new Error('Feed error');
+    const room = dash.in(activeAccountId);
+    room.emit('newJoin', 'dash board joined'); // notify everyone that someone joined
 
-            // close rethinkdb change feed on socket disconnect, not on namespace disconnect
-            socket.on('disconnect', () => {
-              console.log('[ INFO ] SIO DISCONNECT');
-              feed.close();
-            });
+    /*socket.on('sendMessage', (data) => {
+     const { patient, message } = data;
+     twilioClient.sendMessage({
+     to: patient.phoneNumber,
+     from: twilioConfig.number,
+     body: message,
+     // statusCallback: 'https://carecru.ngrok.io/twilio/status',
+     }).then((result) => {
+     // TODO: this is queued, and not delivered, so not techincally sent...
+     console.log(result);
+     // TextMessage.save({
+     //   id: result.sid,
+     //   to: result.to,
+     //   from: result.from,
+     //   body: result.body,
+     //   status: result.status,
+     // }).then(tm => console.log('SMS sent and saved', tm))
+     //   .catch(err => console.log(err));
+     }).catch((err) => {
+     console.log('Error sending SMS');
+     console.log(err);
+     });*/
+  });
 
-            if (doc.getOldValue() === null) {
-              console.log('[ INFO ] CREATE | from=dash; socketId=', socket.id);
-              // io.of('/dash').in(doc.accountId).emit('add:Appointment', doc);
-              socket.emit('add:Appointment', normalize('appointment', doc));
-            } else {
-              // Updated
-              console.log('[ INFO ] UPDATE | from=dash; socketId=', socket.id);
-              // io.of('/dash').in(doc.accountId).emit('add:Appointment', doc);
-              socket.emit('update:Appointment', normalize('appointment', doc));
-            }
-          });
-        });
-    })
-    .on('unauthorized', (msg) => {
-      console.err('unauthorized: ', JSON.stringify(msg.data));
-      throw new Error(msg.data.type);
-    });
-};
+  return dash;
+}
+
+module.exports = setupDashboardNamespace;
