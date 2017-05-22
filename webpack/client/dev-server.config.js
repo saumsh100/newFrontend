3da2ee3bc6b58e98cbd4c33fecad209c4255cb3b
Sergey Skovorodnikov <sergey@carecru.com>
2017-05-22 12:43:32 -0700

    Added defaults to host and port values in webpack config.
    Tested well with and without env vars for port and host.

--- webpack/client/dev-server.config.js
+++ webpack/client/dev-server.config.js
@@ -1,21 +1,19 @@
 const path = require('path');
 const fs = require('fs');
 
-// const { webpackProxyPort = 5100 } = process.env;
+const SERVER_HOST = process.env.SERVER_HOST || 'localhost';
+const SERVER_PORT = process.env.SERVER_PORT || '5000';
 
-const SERVER_PORT = process.env.SERVER_PORT;
-const SERVER_HOST = process.env.SERVER_HOST;
-
-const WP_PROXY_PORT = process.env.WP_PROXY_PORT;
-const WP_PROXY_HOST = process.env.WP_PROXY_HOST;
+const WP_PROXY_HOST = process.env.WP_PROXY_HOST || 'localhost';
+const WP_PROXY_PORT = process.env.WP_PROXY_PORT || '5100';
 
 console.log(`1 server ${SERVER_HOST}:${SERVER_PORT}; proxy ${WP_PROXY_HOST}:${WP_PROXY_PORT}`);
 
 const {
   webpackProxyPort = WP_PROXY_PORT,
   webpackProxyHost = WP_PROXY_HOST,
-  serverHost = 'localhost',
-  serverPort = 5000,
+  serverHost = SERVER_HOST,
+  serverPort = SERVER_PORT,
 } = require('yargs').argv;
 
 console.log(`2 server ${serverHost}:${serverPort}; proxy ${webpackProxyHost}:${webpackProxyPort}`);
