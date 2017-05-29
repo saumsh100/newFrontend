17073b0dfbfd10891f0a6a3264bfb7e55705fc26
Justin Sharp <justin.d.sharp@gmail.com>
2017-05-29 14:08:43 -0600

    fixed server.js file buy removing host

--- server/bin/server.js
+++ server/bin/server.js
@@ -15,6 +15,6 @@ const io = createSocketIOServer(server);
 bindSocketHandlers(io);
 
 // Bind to supplied port
-server.listen(globals.port, globals.host, () => {
-  console.log(`CareCru HTTP Server is running on port ${globals.host}:${globals.port}`);
+server.listen(globals.port, () => {
+  console.log(`CareCru HTTP Server is running on port ${globals.port}`);
 });
