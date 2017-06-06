440ce6bf9b71e70b4f909ff6cce6401d932294d3
Kirat Virk <kirat_virk3@yahoo.ca>
2017-06-06 14:37:41 -0700

    small change to callrail

--- client/components/Intelligence/Business/index.jsx
+++ client/components/Intelligence/Business/index.jsx
@@ -1,42 +1,42 @@
-import React, { PropTypes, Component } from "react";
-import { Card, Col, Grid, Row, ContainerList} from "../../library";
-import colorMap from "../../library/util/colorMap";
+import React, { PropTypes, Component } from 'react';
 import classNames from 'classnames';
 import moment from 'moment';
+import { Card, Col, Grid, Row, ContainerList} from '../../library';
+import colorMap from '../../library/util/colorMap';
 import BusinessStats from './Cards/BusinessStats';
 import DataStats from './Cards/DataStats';
 import Patients from './Cards/Patients';
-import styles from "./styles.scss";
+import styles from './styles.scss';
 
 class Business extends Component {
   render() {
     const data = [
-      {percentage: 12, question: true, count: 353, title: "All Calls", icon: "phone", color: 'primaryInactive' },
-      {percentage: 12, question: true, count: 243, title: "Pickups", icon: "user", color: 'primaryNavyBlue' },
-      {percentage: 12, question: true, count: 102, title: "Bookings", icon: "calendar-o", color: 'primaryDarkBlue' },];
+      {percentage: 12, question: true, count: 353, title: 'All Calls', icon: 'phone', color: 'primaryInactive' },
+      {percentage: 12, question: true, count: 243, title: 'Pickups', icon: 'user', color: 'primaryNavyBlue' },
+      {percentage: 12, question: true, count: 102, title: 'Bookings', icon: 'calendar-o', color: 'primaryDarkBlue' },];
 
-    const tabStep = [{label: "Online Booking", data: {count: '10,104', title: "Website Visits", icon: "television", color: 'primaryColor' }},
-      {label: "Calls From Website", data: {count: 102, title: "Online Booking", icon: "users", color: 'primaryColor' }}, ];
+    const tabStep = [{label: 'Online Booking', data: {count: '10,104', title: 'Website Visits', icon: 'television', color: 'primaryColor' }},
+      {label: 'Calls From Website', data: {count: 102, title: 'Online Booking', icon: 'users', color: 'primaryColor' }}, ];
 
     const patientsData1 = [
-      {count: 5433, title: "Active Patients", date:  moment({year: 2017, month: 2, day: 15}).fromNow(), color: 'primaryColor' },
-      {count: 39, title: "New Patients", date:  moment({year: 2017, month: 1, day: 15}).fromNow(), color: 'primaryBlue' },
-      {count: 1746, title: "Patients with Hygiene Appts", date:  moment({year: 2016, month: 10, day: 10}).fromNow(), color: 'primaryGreen' },
-      {count: "$288", title: "Average Hourly Production", date:  moment({year: 2015, month: 6, day: 15}).fromNow(), color: 'primaryGreen' },];
+      {count: 5433, title: 'Active Patients', date:  moment({year: 2017, month: 2, day: 15}).fromNow(), color: 'primaryColor' },
+      {count: 39, title: 'New Patients', date:  moment({year: 2017, month: 1, day: 15}).fromNow(), color: 'primaryBlue' },
+      {count: 1746, title: 'Patients with Hygiene Appts', date:  moment({year: 2016, month: 10, day: 10}).fromNow(), color: 'primaryGreen' },
+      {count: '$288', title: 'Average Hourly Production', date:  moment({year: 2015, month: 6, day: 15}).fromNow(), color: 'primaryGreen' },];
     const patientsData2 = [
-      {count: 160, title: "Unflled Hours", date:  moment({year: 2017, month: 2, day: 15}).fromNow(), color: 'primaryColor' },
-      {count: 480, title: "Schedule Hours", date:  moment({year: 2017, month: 1, day: 15}).fromNow(), color: 'primaryBlue' },
-      {count: 13, title: "Broken Appts Not Filled", date:  moment({year: 2016, month: 10, day: 10}).fromNow(), color: 'primaryGreen' },
-      {count: "$1300", title: "Revenue Lost From Broken Appts", date:  moment({year: 2015, month: 6, day: 15}).fromNow(), color: 'primaryGreen' },];
+      {count: 160, title: 'Unflled Hours', date:  moment({year: 2017, month: 2, day: 15}).fromNow(), color: 'primaryColor' },
+      {count: 480, title: 'Schedule Hours', date:  moment({year: 2017, month: 1, day: 15}).fromNow(), color: 'primaryBlue' },
+      {count: 13, title: 'Broken Appts Not Filled', date:  moment({year: 2016, month: 10, day: 10}).fromNow(), color: 'primaryGreen' },
+      {count: '$1300', title: 'Revenue Lost From Broken Appts', date:  moment({year: 2015, month: 6, day: 15}).fromNow(), color: 'primaryGreen' },];
 
     const hardcodeData1 = [
-      {count: 202, icon: "phone"},
-      {count: 141, icon: "user"},
-      {count: 71, icon: "calendar-o"},];
+      {count: 202, icon: 'phone'},
+      {count: 141, icon: 'user'},
+      {count: 71, icon: 'calendar-o'},];
     const hardcodeData2 = [
-      {percentage: 2, subtitle: "Calls From Website"},
-      {percentage: 70, subtitle: "Pickups"},
-      {percentage: 53, subtitle: "Booking"},];
+      {percentage: 2, subtitle: 'Calls From Website'},
+      {percentage: 70, subtitle: 'Pickups'},
+      {percentage: 53, subtitle: 'Booking'},];
 
     return (
       <Grid className={styles.business}>
@@ -75,20 +75,20 @@ class Business extends Component {
                     borderColor={colorMap.darkblue}
                     cardTitle="Procedure by Hours"
                     data={[{
-                      title: "Invisalign",
-                      hours: "33,487"
+                      title: 'Invisalign',
+                      hours: '33,487',
                     }, {
-                      title: "Teeth Whitening",
-                      hours: "3,617"
+                      title: 'Teeth Whitening',
+                      hours: '3,617',
                     }, {
-                      title: "Regular Checkup",
-                      hours: "1,901"
+                      title: 'Regular Checkup',
+                      hours: '1,901',
                     }, {
-                      title: "Lost Fillings",
-                      hours: "13,717"
+                      title: 'Lost Fillings',
+                      hours: '13,717',
                     }, {
-                      title: "Emergency Appointments",
-                      hours: "33,487"
+                      title: 'Emergency Appointments',
+                      hours: '33,487',
                     }]}
                   />
                 </Col>
@@ -98,20 +98,20 @@ class Business extends Component {
                     borderColor={colorMap.darkblue}
                     cardTitle="Procedure by Production"
                     data={[{
-                      title: "Invisalign",
-                      hours: "33,487"
+                      title: 'Invisalign',
+                      hours: '33,487',
                     }, {
-                      title: "Teeth Whitening",
-                      hours: "3,617"
+                      title: 'Teeth Whitening',
+                      hours: '3,617',
                     }, {
-                      title: "Regular Checkup",
-                      hours: "1,901"
+                      title: 'Regular Checkup',
+                      hours: '1,901',
                     }, {
-                      title: "Lost Fillings",
-                      hours: "13,717"
+                      title: 'Lost Fillings',
+                      hours: '13,717',
                     }, {
-                      title: "Emergency Appointments",
-                      hours: "33,487"
+                      title: 'Emergency Appointments',
+                      hours: '33,487',
                     }]}
                   />
                 </Col>
