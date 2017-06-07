65c2a154576d9d30b2e37ac986b204d2ab99672e
Sergey Skovorodnikov <sergey@carecru.com>
2017-06-07 12:05:59 -0700

    safety commit for some working tests

--- tests/__tests__/models/createModel/dropTables.js
+++ tests/__tests__/models/createModel/dropTables.js
@@ -10,66 +10,33 @@ function clearTestTables() {
   });
 }
 
-// function dropTestModelTables() {
-//   r.connect(dbConfig, (err, conn) => {
-//     if (err) throw err;
-//     console.log('dropTestModelTables: CONNECTED TEST FILE');
-//
-//     r.db('carecru_development').tableDrop('TestModel').run(conn, (error, res) => {
-//       if (error) throw error;
-//       console.log('dropTestModelTables: ....result', res);
-//     });
-//   });
-// }
-
-// function dropTestModelTables() {
-//   const p = r.connect(dbConfig);
-//   return p.then((conn) => {
-//     console.log('dropTestModelTables: dropping test table');
-//     return r.db('carecru_development').tableDrop('FakeTable').run(conn);
-//   });
-// }
-
-// function listTables() {
-//   r.connect(dbConfig, (err, conn) => {
-//     if (err) throw err;
-//     console.log('listTables: CONNECTED TEST FILE');
-//     r.db('carecru_development').tableList().run(conn, (error, res) => {
-//       if (error) throw err;
-//       console.log('listTables: table list', res);
-//     });
-//   });
-// }
-
+/**
+ * wipe a table
+ * @param tableName
+ */
 function deleteAllFrom(tableName) {
   const p = r.connect(dbConfig);
   return p.then((conn) => {
-    console.log('dropTestModelTables: wiping test table');
+    console.log('dropTestModelTables: wiping test table: ', tableName);
     return r.db('carecru_development').table(tableName).delete().run(conn);
   });
 }
 
-function checkTable(tableName) {
-  const p = r.connect(dbConfig);
-  return p.then((conn) => {
-    return r.db('carecru_development').tableList().contains(tableName).run(conn);
-  });
+function checkTable(conn, tableName) {
+  console.log('checkTable: checking tables');
+  return r.db('carecru_development').tableList().contains(tableName).run(conn); // true|false
 }
 
-// function dropTestModelTables(tableName) {
-//   const p = r.connect(dbConfig);
-//   return p.then((conn) => {
-//     console.log('dropTestModelTables: dropping test table');
-//     return r.db('carecru_development').tableDrop(tableName).run(conn);
-//   });
-// }
-
+/**
+ * works
+ * @param tableName to drop
+ */
 function dropTestModelTables(tableName) {
   const p = r.connect(dbConfig);
   return p.then((conn) => {
     console.log('dropTestModelTables: dropping test table');
     return r.db('carecru_development').tableDrop(tableName).run(conn);
-  }).catch();
+  });
 }
 
 function listTables() {
@@ -82,4 +49,5 @@ function listTables() {
 module.exports = {
   dropTestModelTables,
   listTables,
+  deleteAllFrom,
 };
--- tests/__tests__/models/createModel/index.js
+++ tests/__tests__/models/createModel/index.js
@@ -1,23 +1,62 @@
 
-import thinky from '../../../../server/config/thinky';
 import createModel from '../../../../server/models/createModel';
 import dropTables from './dropTables';
 
+const thinky = require('../../../../server/config/thinky');
+const type = thinky.type;
+
 const dropTestModelTables = dropTables.dropTestModelTables;
 const listTables = dropTables.listTables;
+const deleteAllFrom = dropTables.deleteAllFrom;
 
 const TEST_MODEL = 'TestModel';
+const CARECRU = 'carecru';
+const NOT_CARECRU = 'not_carecru';
+
+function delay(seconds, calculation) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const val = calculation();
+      if (val) return resolve(val);
+      reject();
+    }, seconds);
+  });
+}
 
 describe('#createModel', () => {
 
-  afterEach((done) => {
-    console.log('running after each');
-    dropTestModelTables(TEST_MODEL)
-      .then(() => {
-        console.log('Dropped FakeTable');
+  // afterEach((done) => {
+  //   console.log('running after each');
+  //   dropTestModelTables(TEST_MODEL)
+  //     .then(() => {
+  //       console.log('Dropped FakeTable');
+  //       done();
+  //     })
+  //     .catch(error => console.log('kfdjghdkfjghdkjg', error));
+  //   // return deleteAllFrom(TEST_MODEL);
+  // });
+
+  it('test1', () => {
+    expect(2 + 2).toBe(4);
+  });
+
+  describe('math test 2', () => {
+    it('test2', () => {
+      expect(3 + 3).toBe(6);
+    });
+  });
+
+  describe('math test 2 async', () => {
+    it('async test', (done) => {
+      delay(2000, () => {
+        return 2 * 3;
+      }).then((num) => {
+        expect(num).toBe(6);
+        console.log('test ran');
         done();
-      })
-      .catch(error => console.log('kfdjghdkfjghdkjg', error));
+      });
+    });
+
   });
 
   // it('should be a function', () => {
@@ -28,84 +67,167 @@ describe('#createModel', () => {
   // TODO: test validators...
   // TODO: test requireds, defaults, etc.
 
-  it('shouldRepTheApi', () => {
-    console.log('shouldRepTheApi');
-    const Model = createModel(TEST_MODEL, {
-      homePhoneNumber: thinky.type.string(),
-    }, {
-      aux: {
-        name: {
-          value: 'id',
-        },
-      },
+  describe('Model.docOn(\'saving\')', () => {
+    // afterEach((done) => {
+    //   dropTestModelTables(TEST_MODEL).then(() => done());
+    // });
+
+    it('should call docOn saving hook with Model.save - synchronous', () => {
+      const TestModel = createModel(TEST_MODEL+0, {
+        name: type.string(),
+      });
+
+      const fn = jest.fn();
+      // setup validate function to replace the phone number
+      TestModel.docOn('saving', (doc) => {
+        fn();
+        console.log('.>..... saving');
+      });
+
+      const testModelInstance = { homePhoneNumber: '7782422626' };
+
+      return TestModel.save(testModelInstance)
+        .then((persistedModel) => {
+          expect(fn.mock.calls.length).toBe(1);
+        });
     });
   });
 
-  it('call docOn saving with Model.save', (done) => {
-    console.log('running call docOn saving with Model.save');
-    const TestModel = createModel(TEST_MODEL, {
-      homePhoneNumber: thinky.type.string(),
-    }, {
-      aux: {
-        name: {
-          value: 'id',
-        },
-      },
-    });
-
-    // setup validate function to replace the phone number
-    TestModel.docOn('saving', (doc) => {
-      doc.homePhoneNumber = '111';
-    });
-
-    const testModelInstance = Object.assign({}, { homePhoneNumber: '7782422626' });
-    TestModel.save(testModelInstance)
-      .then((persistedModel) => {
-        expect(persistedModel.id).not.toBeNull();
-        expect(persistedModel.homePhoneNumber).toBe('111');
+  describe('Model.pre(\'save\')', () => {
+    it('it should be called before Model.save - sync', () => {
+      const TestModel = createModel(TEST_MODEL+1, {
+        name: type.string(),
       });
-    done();
+
+      // SYNC pre save call
+      const fn = jest.fn();
+      TestModel.pre('save', function(next) {
+        fn();
+        this.name = NOT_CARECRU;
+        next();
+      });
+
+      const testModelInstance = { name: CARECRU };
+
+      // Model.save
+      return TestModel.save(testModelInstance)
+        .then((persistedModel) => {
+          expect(fn.mock.calls.length).toBe(1);
+          expect(persistedModel.name).toBe(NOT_CARECRU);
+        });
+    });
+
+    it('it should be called before Model.save - async', () => {
+      const TestModel = createModel(TEST_MODEL+2, {
+        name: type.string(),
+      });
+
+      // mock ASYNC function to return a different name value
+      const fn = jest.fn((cb) => {
+        delay(2000, () => {
+          return NOT_CARECRU;
+        }).then((result) => {
+          cb(result);
+        });
+      });
+
+      // ASYNC pre save call
+      TestModel.pre('save', function(next) {
+        fn((newName) => {
+          this.name = newName;
+        });
+        next();
+      });
+
+      const testModelInstance = { name: CARECRU };
+
+      // Model.save
+      return TestModel.save(testModelInstance)
+        .then((persistedModel) => {
+          expect(fn.mock.calls.length).toBe(1);
+          expect(persistedModel.name).toBe(NOT_CARECRU);
+        });
+    });
+
+    // it should be called before creating document then .save()
+    it('it should be called before creating document then .save()', () => {
+      const TestModel = createModel(TEST_MODEL+3, {
+        name: type.string(),
+      });
+
+      // SYNC mock pre save call
+      TestModel.pre('save', function(next) {
+        fn();
+        next();
+      });
+
+      // mock SYNC function
+      const fn = jest.fn();
+
+      // create document
+      const testModelInstance = { name: CARECRU };
+      TestModel.save(testModelInstance)
+        .then((persistedModel) => {
+          console.log('ran: model.save');
+
+          persistedModel.merge({ name: NOT_CARECRU }).save()
+            .then((newPersistedModel) => {
+              expect(fn.mock.calls.length).toBe(2);
+              expect(newPersistedModel.name).toBe(NOT_CARECRU);
+            });
+        });
+    });
+
+    // it should be called before fetching document, merging, then .save()
   });
 
-  test('call docOn saving with Model.merge', (done) => {
-    const TestModel = createModel(TEST_MODEL+2, {
-      homePhoneNumber: thinky.type.string(),
-    }, {
-      aux: {
-        name: {
-          value: 'id',
-        },
-      },
-    });
 
-    // setup validate function to replace the phone number
-    TestModel.docOn('saving', (doc) => {
-      doc.homePhoneNumber = '111';
-    });
+  // TODO this is next
+  /* describe('validateFn', () => {
+    // this should work for us but gets called all the time
+  }); */
 
-    const testModelInstance = Object.assign({}, { homePhoneNumber: '7782422626' });
-    let persistedTestModel = {};
-
-    const prom1 = TestModel.save(testModelInstance)
-      .then((testModel) => {
-        console.log('executing prom1');
-        persistedTestModel = testModel;
-        expect(testModel.id).not.toBeNull();
-        expect(testModel.homePhoneNumber).toBe('111');
-      });
-
-    // need to make sure these are are in order to test correctly
-    const modifiedPersistedTestModel = Object.assign({}, persistedTestModel, { homePhoneNumber: '6045700922' });
-    const prom2 = persistedTestModel.merge(modifiedPersistedTestModel).save()
-      .then((updatedTestModel) => {
-        console.log('executing prom2');
-        expect(testModel.id).not.toBeNull();
-        expect(testModel.homePhoneNumber).toBe('6045700922');
-      });
-
-    Promise.all([prom1, prom2]);
-
-  });
+  // test('call docOn saving with Model.merge', (done) => {
+  //   const TestModel = createModel(TEST_MODEL+2, {
+  //     homePhoneNumber: type.string(),
+  //   }, {
+  //     aux: {
+  //       name: {
+  //         value: 'id',
+  //       },
+  //     },
+  //   });
+  //
+  //   // setup validate function to replace the phone number
+  //   TestModel.docOn('saving', (doc) => {
+  //     doc.homePhoneNumber = '111';
+  //   });
+  //
+  //   const testModelInstance = Object.assign({}, { homePhoneNumber: '7782422626' });
+  //   let persistedTestModel = {};
+  //
+  //   const prom1 = TestModel.save(testModelInstance)
+  //     .then((testModel) => {
+  //       console.log('executing prom1');
+  //       persistedTestModel = testModel;
+  //       expect(testModel.id).not.toBeNull();
+  //       expect(testModel.homePhoneNumber).toBe('111');
+  //     });
+  //
+  //   // need to make sure these are are in order to test correctly
+  //   const modifiedPersistedTestModel = Object.assign({}, persistedTestModel, { homePhoneNumber: '6045700922' });
+  //   const prom2 = persistedTestModel.merge(modifiedPersistedTestModel).save()
+  //     .then((updatedTestModel) => {
+  //       console.log('executing prom2');
+  //       expect(testModel.id).not.toBeNull();
+  //       expect(testModel.homePhoneNumber).toBe('6045700922');
+  //     });
+  //
+  //   // Promise.all([prom1, prom2]).then(() => {
+  //   //   console.log('done');
+  //   //   done();
+  //   // });
+  // });
 });
 
 // function deleteFakeModelsForTests() {
