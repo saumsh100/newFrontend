c6e47bde0d3f93d4d6a1a686b1e885f424ab26b6
Sergey Skovorodnikov <sergey@carecru.com>
2017-06-11 11:30:36 -0700

    skipping batch Patient write test for now

--- tests/__tests__/models/Patient.js
+++ tests/__tests__/models/Patient.js
@@ -189,7 +189,7 @@ describe('Simple patients write test', () => {
   /**
    * - batch create two docs with the same phone number write away
    */
-  test.only('batch create two docs with the same phone number write away', () => {
+  test.skip('batch create two docs with the same phone number write away', () => {
     Object.assign(testPatientObject2, { mobilePhoneNumber: testPatientObject1.mobilePhoneNumber });
     console.log('modified testPatientObject2', testPatientObject2);
     return Patient.save([testPatientObject1, testPatientObject2], { conflict: 'error' })
