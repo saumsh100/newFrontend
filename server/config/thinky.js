0f3a489839391c7c7de01db51d024baf66d5b265
Justin Sharp <justin.d.sharp@gmail.com>
2017-06-16 19:10:52 -0700

    trying one more time

--- server/config/thinky.js
+++ server/config/thinky.js
@@ -5,14 +5,14 @@ const globals = require('./globals');
 
 let dbConfig = globals.db;
 if (globals.env === 'production') {
-  const caCert = fs.readFileSync(`${globals.root}/compose_ca_cert`);
+  const caCert = fs.readFileSync(`${globals.root}/compose_ca_cert`, 'utf8');
   console.log('globals.caCert Buffer');
-  console.log(new Buffer(globals.caCert, 'utf-8'));
+  console.log(new Buffer(globals.caCert));
   console.log('caCert from file');
   console.log(caCert);
   dbConfig = Object.assign({}, dbConfig, {
     ssl: {
-      ca: new Buffer(globals.caCert, 'utf-8'),
+      ca: new Buffer(globals.caCert),
     },
   });
 }
