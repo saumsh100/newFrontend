097bd6e0b29efb497d580e4a80e1e53fb3ca7a5d
Justin Sharp <justin.d.sharp@gmail.com>
2017-06-16 19:00:37 -0700

    changed the env string to be wrapped in a Buffer

--- server/config/thinky.js
+++ server/config/thinky.js
@@ -4,18 +4,15 @@ const createThinky = require('thinky');
 const globals = require('./globals');
 
 let dbConfig = globals.db;
-
-
-
 if (globals.env === 'production') {
   const caCert = fs.readFileSync(`${globals.root}/compose_ca_cert`);
-  console.log('globals.caCert');
-  console.log(globals.caCert);
+  console.log('globals.caCert Buffer');
+  console.log(new Buffer(globals.caCert, 'utf-8'));
   console.log('caCert from file');
   console.log(caCert);
   dbConfig = Object.assign({}, dbConfig, {
     ssl: {
-      ca: caCert,
+      ca: new Buffer(globals.caCert, 'utf-8'),
     },
   });
 }
