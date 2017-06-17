6053a9ec2e5b239d41add894419de3df130730be
Justin Sharp <justin.d.sharp@gmail.com>
2017-06-16 20:03:29 -0700

    removed ca_cert and properly configured the thinky ca

--- server/config/thinky.js
+++ server/config/thinky.js
@@ -1,19 +1,10 @@
 
-const fs = require('fs');
 const createThinky = require('thinky');
 const globals = require('./globals');
 
 let dbConfig = globals.db;
-console.log(dbConfig);
 if (globals.env === 'production') {
-  const caCert = fs.readFileSync(`${globals.root}/compose_ca_cert`, 'utf8');
   const buffer = new Buffer(globals.caCert);
-  console.log('globals.caCert');
-  console.log(globals.caCert);
-  console.log('globals.caCert Buffer');
-  console.log(buffer);
-  console.log('caCert from file');
-  console.log(caCert);
   dbConfig = Object.assign({}, dbConfig, {
     ssl: {
       ca: buffer,
