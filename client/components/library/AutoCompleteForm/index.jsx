9f68bec2f045dfa14ea8e285b4ee3bcd0a8ff523
Kirat Virk <kirat_virk3@yahoo.ca>
2017-06-30 16:38:28 -0700

    debounce for autosuggest

--- client/components/library/AutoCompleteForm/index.jsx
+++ client/components/library/AutoCompleteForm/index.jsx
@@ -1,9 +1,10 @@
 
 import React, { Component, PropTypes } from 'react';
 import Autosuggest from 'react-autosuggest';
-import { Input } from '../';
 import { Provider } from 'react-redux';
 import omit from 'lodash/omit';
+import debounce from 'lodash/debounce';
+import { Input } from '../';
 
 
 // input value for every given suggestion.
@@ -19,31 +20,33 @@ class AutoCompleteForm extends Component {
   constructor() {
     super();
 
+    this.onSuggestionsFetchRequested = this.onSuggestionsFetchRequested.bind(this);
+    this.onSuggestionsClearRequested = this.onSuggestionsClearRequested.bind(this);
+    this.displayField = this.displayField.bind(this);
     this.state = {
       suggestions: [],
     };
 
-    this.onSuggestionsFetchRequested = this.onSuggestionsFetchRequested.bind(this);
-    this.onSuggestionsClearRequested = this.onSuggestionsClearRequested.bind(this);
-    this.displayField = this.displayField.bind(this);
+    this.getSuggestions = debounce(this.onSuggestionsFetchRequested, 300);
   }
 
   // Autosuggest will call this function every time you need to update suggestions.
   // You already implemented this logic above, so just use it.
   onSuggestionsFetchRequested({ value }) {
-    this.props.getSuggestions(value).then((value2) => {
-      this.setState({
-        suggestions: value2,
+    return this.props.getSuggestions(value)
+      .then((value2) => {
+        this.setState({
+          suggestions: value2,
+        });
       });
-    });
-  };
+  }
 
   // Autosuggest will call this function every time you need to clear suggestions.
   onSuggestionsClearRequested() {
     this.setState({
       suggestions: [],
     });
-  };
+  }
 
   displayField(props) {
     delete props.className;
@@ -57,7 +60,7 @@ class AutoCompleteForm extends Component {
     props.value = this.props.value;
 
     return <Input {...props} />;
-  };
+  }
 
   render() {
     const { suggestions } = this.state;
@@ -69,7 +72,7 @@ class AutoCompleteForm extends Component {
         <Autosuggest
           suggestions={suggestions}
           renderInputComponent={this.displayField}
-          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionsFetchRequested={this.getSuggestions}
           onSuggestionsClearRequested={this.onSuggestionsClearRequested}
           renderSuggestion={renderSuggestion}
           {...newProps}
