31be5f67fa16c7d497c241b11a453013cd96a8ae
Justin Sharp <justin.d.sharp@gmail.com>
2017-07-18 18:30:06 -0600

    added better organization

--- tests/__tests__/api/accounts.js
+++ tests/__tests__/api/accounts.js
@@ -16,24 +16,26 @@ describe('/api/accounts', () => {
     token = await generateToken({ username: 'manager@test.com', password: '!@CityOfBudaTest#$' });
   });
 
-  test('GET / - with manager role so only return one', async () => {
-    return request(app)
-      .get('/api/accounts')
-      .set('Authorization', `Bearer ${token}`)
-      .expect(200)
-      .then(({ body }) => {
-        expect(body).toMatchSnapshot();
-      });
-  });
+  describe('GET /', () => {
+    test('with manager role so only return one', async () => {
+      return request(app)
+        .get('/api/accounts')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(200)
+        .then(({ body }) => {
+          expect(body).toMatchSnapshot();
+        });
+    });
 
-  test('GET / - with owner role so return all', async () => {
-    const ownerToken = await generateToken({ username: 'superadmin@test.com', password: '!@CityOfBudaTest#$' })
-    return request(app)
-      .get('/api/accounts')
-      .set('Authorization', `Bearer ${ownerToken}`)
-      .expect(200)
-      .then(({ body }) => {
-        expect(body).toMatchSnapshot();
-      });
-  });
+    test('with owner role so return all', async () => {
+      const ownerToken = await generateToken({ username: 'superadmin@test.com', password: '!@CityOfBudaTest#$' })
+      return request(app)
+        .get('/api/accounts')
+        .set('Authorization', `Bearer ${ownerToken}`)
+        .expect(200)
+        .then(({ body }) => {
+          expect(body).toMatchSnapshot();
+        });
+    });
+  })
 });
