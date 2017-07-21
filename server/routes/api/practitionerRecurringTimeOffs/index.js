a53bb7a003f9db49503c42bf346e38921c5697b5
Kirat Virk <kirat_virk3@yahoo.ca>
2017-07-21 16:38:44 -0700

    syncing

--- server/routes/api/practitionerRecurringTimeOffs/index.js
+++ server/routes/api/practitionerRecurringTimeOffs/index.js
@@ -1,11 +1,15 @@
 import moment from 'moment-timezone';
 
+const union = require('lodash/union');
 const recurringTimeOffRouter = require('express').Router();
+const { r } = require('../../../config/thinky');
 const checkPermissions = require('../../../middleware/checkPermissions');
 const loaders = require('../../util/loaders');
 const normalize = require('../normalize');
-const TimeOff = require('../../../models/PractitionerRecurringTimeOff');
+const RecurringTimeOff = require('../../../models/PractitionerRecurringTimeOff');
 const Account = require('../../../models/Account');
+const Practitioner = require('../../../models/Practitioner');
+const WeeklySchedule = require('../../../models/WeeklySchedule');
 
 
 recurringTimeOffRouter.param('timeOffId', loaders('recurringTimeOff', 'PractitionerRecurringTimeOff'));
@@ -16,7 +20,7 @@ recurringTimeOffRouter.param('timeOffId', loaders('recurringTimeOff', 'Practitio
 recurringTimeOffRouter.get('/', checkPermissions('timeOffs:read'), (req, res, next) => {
 
   // There is no joinData for timeoffs
-  return TimeOff.run()
+  return RecurringTimeOff.run()
     .then(timeOffs => res.send(normalize('practitionerRecurringTimeOffs', timeOffs)))
     .catch(next);
 });
@@ -26,7 +30,7 @@ recurringTimeOffRouter.get('/', checkPermissions('timeOffs:read'), (req, res, ne
  * Create a timeOff
  */
 recurringTimeOffRouter.post('/', checkPermissions('timeOffs:create'), (req, res, next) => {
-  return TimeOff.save(req.body)
+  return RecurringTimeOff.save(req.body)
     .then((tf) => {
       return res.status(201).send(normalize('practitionerRecurringTimeOff', tf))
     })
@@ -37,52 +41,152 @@ recurringTimeOffRouter.post('/', checkPermissions('timeOffs:create'), (req, res,
  * Create a timeOff from PMS
  */
 recurringTimeOffRouter.post('/pms', checkPermissions('timeOffs:create'), (req, res, next) => {
-  const {
-    xmlString,
-    endDate,
-    practitionerId,
-    isWorking,
-    note,
-  } = req.body;
+  const data = req.body;
+  const recurringTimeoffs = [];
+  const chairs = {};
 
-  const {
-    PatternStartDate,
-    StartTime,
-    PatternEndDate,
-    EndTime,
-    interval = 1,
-    DayOfWeek,
-  } = xmlString.RecurrencePattern;
+  for (let i = 0; i < data.length; i++) {
+    const {
+      scheduleJson,
+      endDate,
+      practitionerId,
+      isWorking,
+      chairIds,
+      notes,
+    } = data[i];
 
-  return Account.get(req.body.accountId).run()
-  .then((account) => {
-    if (!isWorking) {
-      const test = moment.tz(PatternEndDate + EndTime, 'MM/DD/YYYY 00:00:00HH:mm:ss', account.timezone);
-      const isInfinity = moment.tz(endDate, 'MM/DD/YYYY HH:mm:ss', account.timezone);
-      const end = test.isBefore(isInfinity.toISOString()) ? isInfinity : test;
-      const start = moment.tz(PatternStartDate + StartTime, 'MM/DD/YYYY 00:00:00HH:mm:ss', account.timezone);
+    const schedule = JSON.parse(scheduleJson);
 
-      const startTime = new Date(1970, 1, 0, start.hours(), start.minutes(), start.seconds());
-      const endTime = new Date(1970, 1, 0, test.hours(), test.minutes(), test.seconds());
+    const {
+      PatternStartDate,
+      StartTime,
+      PatternEndDate,
+      EndTime,
+      interval = 1,
+      DayOfWeek,
+      Id,
+    } = schedule.RecurrencePattern;
 
-      return TimeOff.save({
-        fromPMS: true,
-        dayOfWeek: DayOfWeek,
-        note,
-        allDay: false,
-        interval,
-        startTime: startTime.toISOString(),
-        endTime: endTime.toISOString(),
-        startDate: start.toISOString(),
-        endDate: end.toISOString(),
+    if (isWorking) {
+      if (practitionerId && DayOfWeek && moment(endDate).isAfter(moment('99900620', 'YYYYMMDD').toISOString())) {
+        if (!chairs[practitionerId]) {
+          chairs[practitionerId] = [];
+        }
+
+        chairs[practitionerId].push({
+          Id,
+          practitionerId,
+          chairIds,
+          dayOfWeek: DayOfWeek.toLowerCase(),
+        });
+      }
+    } else {
+      recurringTimeoffs.push({
         practitionerId,
-      })
-      .then(() => {
-        return res.sendStatus(200);
+        PatternStartDate,
+        StartTime,
+        PatternEndDate,
+        EndTime,
+        interval,
+        DayOfWeek,
+        endDate,
+        notes,
       });
     }
+  }
+  // return res.send(200);
 
-    return res.sendStatus(200);
+  return Account.get(req.accountId).run()
+  .then((account) => {
+
+    const allPromises = [];
+    Object.keys(chairs).map((practitionerId) => {
+      const newChairIds = {};
+
+      const ids = {};
+
+      for (let i = 0; i < chairs[practitionerId].length; i++) {
+        const dayOfWeek = chairs[practitionerId][i].dayOfWeek;
+
+        if (!newChairIds[dayOfWeek]) {
+          newChairIds[dayOfWeek] = [];
+        }
+
+        newChairIds[dayOfWeek] = union(newChairIds[dayOfWeek], chairs[practitionerId][i].chairIds);
+        ids[dayOfWeek] = chairs[practitionerId][i].Id;
+      }
+
+      allPromises.push(Practitioner.get(practitionerId).getJoin({ weeklySchedule: true })
+      .then((prac) => {
+        return WeeklySchedule.get(prac.weeklySchedule.id)
+        .then((weeklySchedule) => {
+          Object.keys(newChairIds).map((day) => {
+            weeklySchedule[day].chairIds = newChairIds[day];
+            weeklySchedule[day].pmsScheduleId = ids[day];
+          });
+
+          return weeklySchedule.save();
+        });
+      }));
+    });
+
+    const deleteTimeOffs = Object.keys(recurringTimeoffs).map((allData) => {
+      return RecurringTimeOff
+      .filter(r.row('fromPMS').eq(true))
+      .filter({ practitionerId: allData.practitionerId })
+      .run()
+      .then((timeOffs) => {
+        const promises = [];
+        for (let i = 0; i < timeOffs.length; i++) {
+          promises.push(timeOffs[i].delete());
+        }
+        return Promise.all(promises);
+      });
+    });
+
+    return Promise.all(deleteTimeOffs)
+    .then(() => {
+
+      const waitForAll = [];
+
+      recurringTimeoffs.map((allData) => {
+        const {
+          practitionerId,
+          PatternStartDate,
+          PatternEndDate,
+          StartTime,
+          EndTime,
+          interval,
+          DayOfWeek,
+          endDate,
+          notes,
+        } = allData;
+
+        const test = moment.tz(PatternEndDate + EndTime, 'MM/DD/YYYY 00:00:00HH:mm:ss', account.timezone);
+        const isInfinity = moment.tz(endDate, account.timezone).subtract(1, 'days');
+
+        const end = test.isBefore(isInfinity.toISOString()) ? isInfinity : test;
+        const start = moment.tz(PatternStartDate + StartTime, 'MM/DD/YYYY 00:00:00HH:mm:ss', account.timezone);
+
+        const startTime = new Date(1970, 1, 0, start.hours(), start.minutes(), start.seconds());
+        const endTime = new Date(1970, 1, 0, test.hours(), test.minutes(), test.seconds());
+
+        waitForAll.push(RecurringTimeOff.save({
+          fromPMS: true,
+          dayOfWeek: DayOfWeek,
+          note: notes,
+          allDay: false,
+          interval,
+          startTime: startTime.toISOString(),
+          endTime: endTime.toISOString(),
+          startDate: start.toISOString(),
+          endDate: end.toISOString(),
+          practitionerId,
+        }));
+      });
+
+      return Promise.all(waitForAll);
+    }).then(() => res.sendStatus(200));
   })
   .catch(next);
 });
