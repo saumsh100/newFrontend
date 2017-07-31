6c2c87b67f7d7acc0540666e2585055bd0a7e6af
Gavin Servai <gavinservai@gmail.com>
2017-07-31 12:44:52 -0700

    [api-sequelize] Added reminders test

--- server/routes/_api/reminders/index.js
+++ server/routes/_api/reminders/index.js
@@ -3,7 +3,7 @@ import checkPermissions from '../../../middleware/checkPermissions';
 import { sequelizeLoader } from '../../util/loaders';
 import normalize from '../normalize';
 import { Reminder } from '../../../_models';
-import { StatusError } from '../../../util/StatusError';
+import StatusError from '../../../util/StatusError';
 
 const remindersRouter = Router();
 
@@ -17,18 +17,59 @@ remindersRouter.get('/:accountId/reminders', checkPermissions('accounts:read'), 
   if (req.account.id !== req.accountId) {
     next(StatusError(403, 'req.accountId does not match URL account id'));
   }
-  console.log('going to try');
+
   try {
     const reminders = await Reminder.findAll({
       raw: true,
       where: { accountId: req.accountId },
     });
-    console.log(reminders);
+
     res.send(normalize('reminders', reminders));
   } catch (error) {
     next(error);
   }
-
 });
+
+/**
+ * POST /:accountId/reminders
+ */
+remindersRouter.post('/:accountId/reminders', checkPermissions('accounts:read'), (req, res, next) => {
+  if (req.account.id !== req.accountId) {
+    next(StatusError(403, 'req.accountId does not match URL account id'));
+  }
+
+  const saveReminder = Object.assign({ accountId: req.accountId }, req.body);
+
+  return Reminder.create(saveReminder)
+    .then(reminder => res.status(201).send(normalize('reminder', reminder.dataValues)))
+    .catch(next);
+});
+
+/**
+ * PUT /:accountId/reminders/:reminderId
+ */
+remindersRouter.put('/:accountId/reminders/:reminderId', checkPermissions('accounts:read'), (req, res, next) => {
+  if (req.accountId !== req.account.id) {
+    return next(StatusError(403, 'Requesting user\'s activeAccountId does not match account.id'));
+  }
+
+  return req.reminder.update(req.body)
+    .then(reminder => res.send(normalize('reminder', reminder.dataValues)))
+    .catch(next);
+});
+
+/**
+ * DELETE /:accountId/reminders/:reminderId
+ */
+remindersRouter.delete('/:accountId/reminders/:reminderId', checkPermissions('accounts:read'), (req, res, next) => {
+  if (req.accountId !== req.account.id) {
+    return next(StatusError(403, 'Requesting user\'s activeAccountId does not match account.id'));
+  }
+
+  return req.reminder.destroy()
+    .then(() => res.status(204).send())
+    .catch(next);
+});
+
 
 module.exports = remindersRouter;
