fb1995f3d541089debd237aac7cc5ac431ab9de0
jatd <dhillon.jatinder@gmail.com>
2017-08-15 12:25:17 -0700

    fixes to typography page

--- client/components/Typography/index.jsx
+++ client/components/Typography/index.jsx
@@ -1,95 +1,219 @@
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import styles from './styles.scss';
-import classNames from 'classnames'
-import { Card, CodeSnippet } from '../library';
-export default function Typography(props) {
-  console.log(classNames(styles.padding))
+import classNames from 'classnames';
+import { push } from 'react-router-redux';
+import { Card, CodeSnippet, Header } from '../library';
+
+function Typography(props) {
+  const {
+    push
+  } = props;
 
   return (
     <Card className={styles.mainContainer}>
       <div className={styles.padding}>
-        Default font: abcdefg 12345 ABCDEFG
+        <span
+          className={styles.fontMedium}
+        >
+          .fontMedium
+        </span>
         <CodeSnippet
-          codeSnippet={'{font-family: Gotham-Book; font-size: 14px; font-weight: 500;}'}
+          codeSnippet={'{font-family: Gotham Medium; font-size: 14px; font-weight: 500;}'}
           hideClipBoard
+        />
+      </div>
 
-        />
-      </div>
       <div className={styles.padding}>
-        <span className={styles.mediumFont}> Medium font: abcdefg 12345 ABCDEFG </span>
-        <div>
-          <CodeSnippet
-            className={styles.padding}
-            codeSnippet={'{font-family: Gotham Medium; font-size: 14px; font-weight: 500;}'}
-            hideClipBoard
-          />
+        <span
+          className={styles.fontJumbo}
+        >
+          .fontJumbo
+        </span>
+        <CodeSnippet
+          codeSnippet={'{font-family: Gotham Medium; font-size: 73px; font-weight: 600; letter-spacing: 0.5px;}'}
+          hideClipBoard
+        />
+        <div className={styles.examples} >
+          <span>- dashboard-stats, intelligence-stats, or business-stats.</span>
         </div>
       </div>
-      <div className={styles.padding}>
-        <span className={styles.jumboFont}> Jumbo font: abcdefg 12345 ABCDEFG  </span>
-        <CodeSnippet
-          className={styles.padding}
-          codeSnippet={'{font-family: Gotham Medium; font-size: 73px; font-weight: 600; letter-spacing: 2px;}'}
-          hideClipBoard
 
-        />
-      </div>
       <div className={styles.padding}>
-        <span className={styles.header}> Header : abcdefg 12345 ABCDEFG - Settings Page </span>
+        <span
+          className={styles.header}
+        >
+          .header
+        </span>
         <CodeSnippet
-          className={styles.padding}
-          codeSnippet={'{font-size: 25px; font-weight: 600; letterSpacing: 2px;}'}
+          codeSnippet={'{font-size: 25px; font-weight: 600; letterSpacing: 0.5px;}'}
           hideClipBoard
         />
-      </div>
-      <div className={styles.padding}>
-        <span className={styles.cardHeaderCount}> CardHeader Count: 12345 - Dashboard Cards </span>
-        <CodeSnippet
-          className={styles.padding}
-          codeSnippet={'{font-size: 25px; font-weight: 600; letterSpacing: 2px;}'}
-          hideClipBoard
-        />
-        <div>
-          <span className={styles.cardHeaderTitle}> CardHeader Title: abcdefg 12345 ABCDEFG - Dashboard Cards </span>
-        </div>
-        <CodeSnippet
-          className={styles.padding}
-          codeSnippet={'{font-family: Gotham Medium; font-size: 16px; letterSpacing: 2px;}'}
-          hideClipBoard
-        />
-      </div>
-      <div className={styles.padding}>
-        <span className={styles.listItemHeader}> List Header: abcdefg 12345 ABCDEFG - Appointments List  </span>
-        <CodeSnippet
-          className={styles.padding}
-          codeSnippet={'{font-family: Gotham Medium; color: #2e3845; letterSpacing: 2px;}'}
-          hideClipBoard
-        />
-      </div>
-      <div className={styles.padding}>
-        <span className={styles.subHeader}> Sub Header: abcdefg 12345 ABCDEFG - Patientlist on Patient Management  </span>
-        <CodeSnippet
-          className={styles.padding}
-          codeSnippet={'{color: darken(lightgrey, 5%); letterSpacing: 2px;}'}
-          hideClipBoard
-        />
-        <div>
-          <span className={styles.subHeaderMedium}> Sub Header Medium: abcdefg 12345 ABCDEFG - Patientlist on Patient Management  </span>
-          <CodeSnippet
-            className={styles.padding}
-            codeSnippet={'{color: darken(lightgrey, 5%); font-size: 12px letterSpacing: 2px;}'}
-            hideClipBoard
-          />
+        <div className={styles.examples} >
+          <span>- all settings page headers eg. General.</span>
         </div>
       </div>
+
       <div className={styles.padding}>
-        <span className={styles.tab}> Tabs: abcdefg 12345 ABCDEFG - Settings Practitioner  </span>
+        <span
+          className={styles.cardHeaderCount}
+        >
+          .cardHeaderCount
+        </span>
         <CodeSnippet
-          className={styles.padding}
+          codeSnippet={'{font-size: 25px; font-weight: 600; letterSpacing: 0.5px;}'}
+          hideClipBoard
+        />
+        <div className={styles.examples} >
+          <span>- the displayed number on dashboard-appointment-requests.</span>
+        </div>
+      </div>
+
+      <div className={styles.padding}>
+        <span
+          className={styles.cardHeaderTitle}
+        >
+          .cardHeaderTitle
+        </span>
+        <CodeSnippet
+          codeSnippet={'{font-family: Gotham Medium; font-size: 16px; letterSpacing: 0.5px;}'}
+          hideClipBoard
+        />
+        <div className={styles.examples} >
+          <span>- the displayed title on dashboard-appointment-requests.</span>
+        </div>
+      </div>
+
+      <div className={styles.padding}>
+        <span
+          className={styles.listItemHeader}
+        >
+          .listItemHeader
+        </span>
+        <CodeSnippet
+          codeSnippet={'{font-family: Gotham Medium; color: #2e3845; letterSpacing: 0.5px;}'}
+          hideClipBoard
+        />
+        <div className={styles.examples} >
+          <span>- patient name on dashboard-sentReminders-list or practitioner name on settings-practitioners-list .</span>
+        </div>
+      </div>
+
+      <div className={styles.padding}>
+        <span
+          className={styles.subHeader}
+        >
+          .subHeader
+        </span>
+        <CodeSnippet
+          codeSnippet={'{color: darken(lightgrey, 5%); letterSpacing: 0.5px;}'}
+          hideClipBoard
+        />
+        <div className={styles.examples} >
+          <span>- service name on dashboard-appointment-list .</span>
+        </div>
+      </div>
+
+      <div className={styles.padding}>
+        <span
+          className={styles.subHeaderSmall}
+        >
+          .subHeaderSmall
+        </span>
+        <CodeSnippet
+          codeSnippet={`{color: $primary-darkgrey; font-size: $small-size; @extend .letterSpacing;}`}
+          hideClipBoard
+        />
+        <div className={styles.examples} >
+          <span>- text message on patients-messages-list, below the name .</span>
+        </div>
+      </div>
+
+      <div className={styles.padding}>
+        <span
+          className={styles.subHeaderExtraSmall}
+        >
+          .subHeaderExtraSmall
+        </span>
+        <CodeSnippet
+          codeSnippet={`{color: $primary-darkgrey;font-size: $extra-small-size;@extend .letterSpacing;}`}
+          hideClipBoard
+        />
+        <div className={styles.examples} >
+          <span>- last appointment text on large patient-info display .</span>
+        </div>
+      </div>
+
+      <div className={styles.padding}>
+        <span
+          className={styles.subHeaderMedium}
+        >
+          .subHeaderMedium
+        </span>
+        <CodeSnippet
+          codeSnippet={'{color: darken(lightgrey, 5%); font-size: 12px letterSpacing: 0.5px;}'}
+          hideClipBoard
+        />
+        <div className={styles.examples} >
+          <span>- phone number and email on large patient-info display .</span>
+        </div>
+      </div>
+
+      <div className={styles.padding}>
+        <span
+          className={styles.subHeaderMediumSmall}
+        >
+          .subHeaderMediumSmall
+        </span>
+        <CodeSnippet
+          codeSnippet={`{font-family: $medium-font;font-size: $small-size;color: $primary-darkgrey;@extend .letterSpacing;}`}
+          hideClipBoard
+        />
+        <div className={styles.examples} >
+          <span>- next appt text on patients-list, below name.</span>
+        </div>
+      </div>
+
+      <div className={styles.padding}>
+        <span
+          className={styles.tab}
+        >
+          .tab
+        </span>
+        <CodeSnippet
           codeSnippet={'{ font-family: Gotham Medium; color: #959596; }'}
           hideClipBoard
         />
+        <div className={styles.examples} >
+          <span>- settings sub-pages and setting-practitioners.</span>
+        </div>
+      </div>
+
+      <div className={styles.legend}>
+        <div> <b>TYPOGRAPHY</b></div><br/>
+        <span><b>How to Use These Fonts</b></span><br /> <br />
+        <div>
+          <span><b>Option 1</b></span><br />
+          <div>- Import the font.scss file.</div><br />
+          <div>- Extend the font class into your local style sheet (eg: @extend .jumboFont).</div>
+        </div><br />
+        <div>
+          <span><b>Option 2</b></span><br />
+          <div>- No import required</div><br />
+          <div>- In your local style sheet use composes function. (eg: composes: jumboFont from "sass-loader!../../font.scss")</div>
+        </div>
       </div>
     </Card>
   );
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    push,
+  }, dispatch);
+}
+
+const enhance = connect(null, mapDispatchToProps);
+
+export default enhance(Typography);
