db7b583b37ee14cd6e57aeda94c22410c3e7dcce
Justin Sharp <justin.d.sharp@gmail.com>
2017-08-16 16:10:22 -0600

    cleaned up some styles

--- client/components/LastSyncDisplay/index.jsx
+++ client/components/LastSyncDisplay/index.jsx
@@ -7,9 +7,7 @@ import styles from './styles.scss';
 
 export default function LastSyncDisplay(props) {
   const { date, className } = props;
-
   if (!date) return null;
-
   const mDate = moment(date);
   const isOff = mDate.isBefore(moment().subtract(10, 'minutes'));
   const statusClass = isOff ? styles.off : styles.on;
@@ -21,3 +19,8 @@ export default function LastSyncDisplay(props) {
     </div>
   );
 }
+
+LastSyncDisplay.propTypes = {
+  date: PropTypes.string,
+  className: PropTypes.string,
+};
