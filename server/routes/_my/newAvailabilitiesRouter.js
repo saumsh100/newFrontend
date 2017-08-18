78c67d24ac1f51f48815be48e51fb5520dd5e2db
Justin Sharp <justin.d.sharp@gmail.com>
2017-08-18 16:06:28 -0600

    added the Next Availability button to the availabilities display

--- server/routes/_my/newAvailabilitiesRouter.js
+++ server/routes/_my/newAvailabilitiesRouter.js
@@ -1,4 +1,5 @@
 
+import moment from 'moment';
 import { Router } from 'express';
 import { fetchAvailabilities } from '../../lib/_availabilities';
 import { sequelizeLoader } from '../util/loaders';
@@ -10,16 +11,35 @@ availabilitiesRouter.param('accountId', sequelizeLoader('account', 'Account'));
 /**
  * Availabilities API
  */
-availabilitiesRouter.get('/accounts/:accountId/availabilities', (req, res, next) => {
-  const data = Object.assign({}, req.query, {
-    accountId: req.account.id,
-    timeInterval: req.account.timeInterval,
-  });
-  return fetchAvailabilities(data)
-    .then((availabilities) => {
-      res.send({ availabilities });
-    })
-    .catch(next);
+availabilitiesRouter.get('/accounts/:accountId/availabilities', async (req, res, next) => {
+  try {
+    const data = Object.assign({}, req.query, {
+      accountId: req.account.id,
+      timeInterval: req.account.timeInterval,
+    });
+
+    // Fetch availabilities for date range
+    const availabilities = await fetchAvailabilities(data);
+    if (availabilities.length) {
+      res.send({
+        availabilities,
+        nextAvailability: availabilities[0],
+      });
+    } else {
+      // Since there's no availabilities, let's search a bit further to grab nextAvailability
+      // Yes its a naive attempt, will be better to refactor availabilities first,
+      // and have a function that does an early return
+      data.startDate = data.endDate;
+      data.endDate = moment(data.endDate).add(3, 'weeks').toISOString();
+      const more = await fetchAvailabilities(data);
+      res.send({
+        availabilities: [],
+        nextAvailability: more.length ? more[0] : null,
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = availabilitiesRouter;
