92b2081a235881a90d3f62e1bb6fb2694471b916
jatd <dhillon.jatinder@gmail.com>
2017-08-22 09:16:26 -0700

    added model

---
+++ client/entities/models/Alert.js
@@ -0,0 +1,17 @@
+
+import createModel from '../createModel';
+
+const AlertsSchema = {
+  id: null,
+  title: null,
+  body: null,
+  type: null,
+  status: null,
+  time: null,
+  sticky: null,
+  action: null,
+};
+
+export default class Alerts extends createModel(AlertsSchema) {
+
+}
