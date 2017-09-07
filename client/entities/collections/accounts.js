ebfe78bdc0034f2e31fb41f9183fe786cc842942
Justin Sharp <justin.d.sharp@gmail.com>
2017-09-06 21:11:59 -0600

    quick hotfix for an issue i saw on another branch

--- client/entities/collections/accounts.js
+++ client/entities/collections/accounts.js
@@ -6,7 +6,7 @@ export default class accounts extends createCollection(Account) {
   getUrlRoot(base) {
     const { auth } = window.store.getState();
     const baseUrl = '/api/accounts';
-    return base ? baseUrl : `${base}/${auth.get('accountId')}`;
+    return base ? baseUrl : `${baseUrl}/${auth.get('accountId')}`;
   }
 
 }
