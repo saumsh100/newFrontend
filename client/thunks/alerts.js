8dc7abc5528577af86dee5bfd45d49a3a45fe52f
jatd <dhillon.jatinder@gmail.com>
2017-09-08 16:02:59 -0700

    code clean up

--- client/thunks/alerts.js
+++ client/thunks/alerts.js
@@ -36,16 +36,16 @@ function notify(alert, type) {
   };
 
   // Let's check whether notification permissions have already been granted
-  if (Notification.permission === "granted") {
+  if (Notification.permission === 'granted') {
     // If it's okay let's create a notification
     return new Notification(title, options);
   }
 
   // Otherwise, we need to ask the user for permission
-  else if (Notification.permission !== "denied") {
+  else if (Notification.permission !== 'denied') {
     Notification.requestPermission(function (permission) {
       // If the user accepts, let's create a notification
-      if (permission === "granted") {
+      if (permission === 'granted') {
         return new Notification(title, options);
       }
     });
