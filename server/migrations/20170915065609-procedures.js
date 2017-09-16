6c8824c92295fad0c8829f25726de8f28582a786
Kirat Virk <kirat_virk3@yahoo.ca>
2017-09-15 17:30:15 -0700

    fix procedures

--- server/migrations/20170915065609-procedures.js
+++ server/migrations/20170915065609-procedures.js
@@ -1,12 +1,19 @@
 'use strict';
 
 const procedures = require('../fixtures/procedures/procedureDump.json');
+const uuid = require('uuid').v4;
 
 module.exports = {
   up: async function (queryInterface) {
+    const syncProcedures = procedures.map((procedure) => {
+      return Object.assign({}, procedure, {
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    });
     return queryInterface.bulkInsert(
       'Procedures',
-      procedures,
+      syncProcedures,
     );
   },
 
