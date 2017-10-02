0bdc2df7270984e873d17c09de09ade539aef230
Justin Sharp <justin.d.sharp@gmail.com>
2017-10-02 10:11:07 -0600

    added the editable configuration values list

---
+++ client/components/Admin/Connectors/Advanced/ConfigurationItem.jsx
@@ -0,0 +1,53 @@
+
+import React, { Component, PropTypes } from 'react';
+import { Form, Field } from '../../../library';
+import styles from './config-item.scss';
+
+export default class ConfigurationItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleValueChange = this.handleValueChange.bind(this);
+  }
+
+  handleValueChange({ value }) {
+    const { account, configuration } = this.props;
+    const { attributes } = configuration;
+    const { name } = attributes;
+    // Put together configuration object and call parent function
+    if (confirm(`Are you sure you want to change ${name} in ${account.name}?`)) {
+      this.props.onUpdateConfiguration({ name, value });
+    }
+  }
+
+  render() {
+    const { configuration } = this.props;
+    const { attributes } = configuration;
+    const initialValues = { value: attributes.value };
+    return (
+      <div className={styles.configWrapper}>
+        <div className={styles.name}>
+          {attributes.name}
+        </div>
+        <div className={styles.description}>
+          {attributes.description}
+        </div>
+        <Form
+          initialValues={initialValues}
+          onSubmit={this.handleValueChange}
+          form={`configuration_value_${configuration.id}`}
+        >
+          <Field
+            label="Value"
+            name="value"
+          />
+        </Form>
+      </div>
+    );
+  }
+}
+
+ConfigurationItem.propTypes = {
+  account: PropTypes.object,
+  configuration: PropTypes.object,
+};
