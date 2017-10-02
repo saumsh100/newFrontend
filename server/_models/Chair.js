c7144057a25ed496997f6944bbd5fd158da7dce1
Kirat Virk <kirat_virk3@yahoo.ca>
2017-10-02 15:02:56 -0700

    fix for practitioners post

--- server/_models/Chair.js
+++ server/_models/Chair.js
@@ -19,6 +19,7 @@ export default function (sequelize, DataTypes) {
     pmsId: {
       type: DataTypes.STRING,
       validate: {
+        // validator for if pmsId and accountId are a unique combo
         isUnique(value, next) {
           return Chair.findOne({
             where: {
