0b608fda96a559af813ef83a5c9520135cb27e58
Kirat Virk <kirat_virk3@yahoo.ca>
2017-10-05 13:48:01 -0700

    removeFunction

--- client/components/Intelligence/Business/index.jsx
+++ client/components/Intelligence/Business/index.jsx
@@ -14,27 +14,7 @@ import BusinessStats from './Cards/BusinessStats';
 import Patients from './Cards/Patients';
 import styles from './styles.scss';
 import stylesOverview from '../Overview/styles.scss';
-
-function nFormatter(num, digits) {
-  num = Number(num);
-  const si = [
-    { value: 1E18, symbol: 'E' },
-    { value: 1E15, symbol: 'P' },
-    { value: 1E12, symbol: 'T' },
-    { value: 1E9, symbol: 'G' },
-    { value: 1E6, symbol: 'M' },
-    { value: 1E3, symbol: 'K' },
-  ];
-  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/i;
-  for (let i = 0; i < si.length; i++) {
-    if (num >= si[i].value) {
-      return (num / si[i].value).toFixed(digits).replace(rx, '$1') + si[i].symbol;
-    }
-  }
-
-  return num.toFixed(digits).replace(rx, '$1');
-}
-
+import nFormatter from '../nFormatter';
 
 class Business extends Component {
 
