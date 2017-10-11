e0e866e2e8f53678e0b830277451c31d07eff29f
Justin Sharp <justin.d.sharp@gmail.com>
2017-10-11 12:42:48 -0600

    added some better insights into comms jobs on NASA page

--- client/components/Admin/Connectors/Advanced/index.jsx
+++ client/components/Admin/Connectors/Advanced/index.jsx
@@ -2,7 +2,12 @@
 import React, { Component, PropTypes } from 'react';
 import axios from 'axios';
 import findIndex from 'lodash/findIndex';
+import isEmpty from 'lodash/isEmpty';
 import ConfigurationItem from './ConfigurationItem';
+import {
+  List,
+  ListItem,
+} from '../../../library';
 import styles from './styles.scss';
 
 const headersConfig = {
@@ -10,6 +15,49 @@ const headersConfig = {
     'Accept': 'application/vnd.api+json',
   },
 };
+
+function RemindersStats({ reminders }) {
+  return (
+    <List>
+      {reminders.map((reminder) => {
+        return (
+          <ListItem key={reminder.id} className={styles.listItem}>
+            <div className={styles.col}>Type: {reminder.primaryType}</div>
+            <div className={styles.col}>Length: {reminder.lengthSeconds}</div>
+            <div className={styles.col}>Success: {reminder.success}</div>
+            <div className={styles.col}>Fail: {reminder.fail}</div>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+}
+
+function RecallsStats({ recalls }) {
+  return (
+    <List>
+      {recalls.map((recall) => {
+        return (
+          <ListItem key={recall.id} className={styles.listItem}>
+            <div className={styles.col}>Type: {recall.primaryType}</div>
+            <div className={styles.col}>Length: {recall.lengthSeconds}</div>
+            <div className={styles.col}>Success: {recall.success}</div>
+            <div className={styles.col}>Fail: {recall.fail}</div>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+}
+
+function ReviewsStats({ reviews }) {
+  return (
+    <div>
+      <div>Success: {reviews.success}</div>
+      <div>Fail: {reviews.fail}</div>
+    </div>
+  );
+}
 
 export default class Advanced extends Component {
   constructor(props) {
@@ -20,6 +68,8 @@ export default class Advanced extends Component {
       isLoading: false,
       configurations: [],
       recalls: [],
+      reminders: [],
+      reviews: null,
     };
 
     this.onUpdateConfiguration = this.onUpdateConfiguration.bind(this);
@@ -31,13 +81,18 @@ export default class Advanced extends Component {
     const { account } = this.props;
     return Promise.all([
         axios.get(`/api/accounts/${account.id}/configurations`, headersConfig),
-        axios.get(`/api/accounts/${account.id}/recalls/stats`)
+        axios.get(`/api/accounts/${account.id}/reminders/stats`),
+        axios.get(`/api/accounts/${account.id}/recalls/stats`),
+        axios.get(`/api/accounts/${account.id}/reviews/stats`),
       ])
-      .then(([{ data }, recallsData]) => {
+      .then(([{ data }, remindersData, recallsData, reviewsData]) => {
+        console.log(reviewsData);
         this.setState({
           isLoading: false,
           configurations: data.data,
           recalls: recallsData.data,
+          reminders: remindersData.data,
+          reviews: reviewsData.data,
         });
       })
       .catch(err => console.error('Failed to load configs', err));
@@ -59,21 +114,27 @@ export default class Advanced extends Component {
 
   render() {
     const { account } = this.props;
-    const { configurations, recalls } = this.state;
+    const { configurations, recalls, reminders, reviews } = this.state;
     return (
       <div className={styles.advancedWrapper}>
+        <h3>Reminders</h3>
+        {!reminders.length ? <div>No Reminders</div> :
+          <RemindersStats
+            reminders={reminders}
+          />
+        }
         <h3>Recalls</h3>
-        {!recalls.length ? <div>No Recalls</div> : null}
-        {recalls.map((recall) => {
-          return (
-            <div>
-              <div>Type: {recall.primaryType}</div>
-              <div>Length: {recall.lengthSeconds}</div>
-              <div>Success: {recall.success}</div>
-              <div>Fail: {recall.fail}</div>
-            </div>
-          );
-        })}
+        {!recalls.length ? <div>No Recalls</div> :
+          <RecallsStats
+            recalls={recalls}
+          />
+        }
+        <h3>Reviews</h3>
+        {!reviews ? <div>No Reviews</div> :
+          <ReviewsStats
+            reviews={reviews}
+          />
+        }
         <h3>Configurations</h3>
         {configurations.map((config) => {
           return (
