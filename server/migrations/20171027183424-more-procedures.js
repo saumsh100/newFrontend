f2feeff946b3fa06046116d653e3aa51ca4e65df
Kirat Virk <kirat_virk3@yahoo.ca>
2017-10-27 15:07:23 -0700

    added more procedures

---
+++ server/migrations/20171027183424-more-procedures.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const procedures = require('../fixtures/procedures/procedureDumpV2.json');
+
+module.exports = {
+  up: async function (queryInterface) {
+    const syncProcedures = procedures.map((procedure) => {
+      return Object.assign({}, procedure, {
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    });
+    return queryInterface.bulkInsert(
+      'Procedures',
+      syncProcedures,
+    );
+  },
+
+  down: function (queryInterface) {
+    queryInterface.bulkDelete('DeliveredProcedures', null, {});
+    return queryInterface.bulkDelete('Procedures', null, {});
+  }
+};
