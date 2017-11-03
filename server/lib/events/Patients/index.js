f079ccd6a8a6150073be319a67be34da84a42952
jatd <dhillon.jatinder@gmail.com>
2017-11-03 09:39:38 -0700

    more optimizing

--- server/lib/events/Patients/index.js
+++ server/lib/events/Patients/index.js
@@ -36,6 +36,51 @@ function getFirstNextLastAppointment(app) {
   });
 };
 
+function optimizedFirstNextLastSetter(app, patient, startDate) {
+  if (moment(startDate).isAfter(new Date()) && !patient.nextApptId) {
+
+    patient.nextApptId = app.id;
+
+    return Patient.update(patient, {
+      where: {
+        id: patient.id,
+      },
+    });
+
+  } else if (moment(startDate).isAfter(new Date()) && patient.nextApptId) {
+
+    return Appointment.findOne({
+      where: { id: patient.nextApptId },
+      raw: true,
+    }).then((appNext) => {
+      if (moment(appNext.startDate).isAfter(moment(startDate))) {
+        patient.nextApptId = app.id;
+
+        return Patient.update(patient, {
+          where: {
+            id: patient.id,
+          },
+        });
+      }
+    });
+
+  } else if (moment(startDate).isBefore(new Date())
+    && !patient.lastApptId && !patient.firstApptId) {
+
+    patient.lastApptId = app.id;
+    patient.firstApptId = app.id;
+
+    return Patient.update(patient, {
+      where: {
+        id: patient.id,
+      },
+    });
+
+  } else {
+    return getFirstNextLastAppointment(app);
+  }
+}
+
 function registerFirstNextLastCalc(sub, io) {
   sub.on('data', (data) => {
     Appointment.findOne({
@@ -52,71 +97,12 @@ function registerFirstNextLastCalc(sub, io) {
     }).then((app) => {
       const patient = app.patient;
       const startDate = app.startDate;
-      console.log(patient)
 
-     if (!app.isDeleted && !app.isPending && !app.isCancelled) {
+      if (!app.isDeleted && !app.isPending && !app.isCancelled) {
+        return optimizedFirstNextLastSetter(app, patient, startDate);
+      }
 
-       if (moment(startDate).isAfter(new Date()) && !patient.nextApptId ) {
-
-         patient.nextApptId = app.id;
-         console.log('patient--->', patient)
-
-         return Patient.update(patient, {
-           where: {
-             id: patient.id,
-           },
-         });
-
-       } else if (moment(startDate).isAfter(new Date()) && patient.nextApptId) {
-
-         Appointment.findOne({
-           where: { id: patient.nextApptId },
-           raw: true,
-         }).then((appNext) => {
-            if (moment(appNext.startDate).isAfter(moment(startDate))) {
-              patient.nextApptId = app.id;
-              console.log('patient--->', patient)
-
-              return Patient.update(patient, {
-                where: {
-                  id: patient.id,
-                },
-              });
-            }
-         });
-
-       } else if (moment(startDate).isBefore(new Date()) && !patient.lastApptId && !patient.firstApptId) {
-         patient.lastApptId = app.id;
-         patient.firstApptId = app.id;
-
-         return Patient.update(patient, {
-           where: {
-             id: patient.id,
-           },
-         });
-       } else if (moment(startDate).isBefore(new Date()) && patient.lastApptId && patient.firstApptId) {
-
-         Appointment.findOne({
-           where: { id: patient.lastApptId },
-           raw: true,
-         }).then((appLast) => {
-           if (moment(appLast.startDate).isBefore(moment(startDate))) {
-             patient.lastApptId = app.id;
-
-             return Patient.update(patient, {
-               where: {
-                 id: patient.id,
-               },
-             });
-           }
-         });
-
-       } else {
-         return getFirstNextLastAppointment(app);
-       }
-     } else {
-       return getFirstNextLastAppointment(app);
-     }
+      return getFirstNextLastAppointment(app);
     });
   });
 }
