4bd7b73dc154e81011dfe6e4db9b1d57fbda0d83
jatd <dhillon.jatinder@gmail.com>
2017-11-07 16:23:31 -0800

    documenting firstNextLastAppointment calculation function

--- server/lib/firstNextLastAppointment.js
+++ server/lib/firstNextLastAppointment.js
@@ -1,8 +1,19 @@
 const moment = require('moment');
 
+/**
+ * CalcFirstNextLastAppointment calculates the first/last/next appointment for a patient
+ * - from a list of appointments that are not cancelled, deleted, or pending
+ * - sorted by patientId and startDate descending
+ * - updates the patient through the callback function
+ *
+ * @param apps
+ * @param callback
+ */
+
 function CalcFirstNextLastAppointment(apps, callback) {
   const today = new Date();
 
+  // loops through the entire set of appointments
   let j = 0;
   while (j < apps.length) {
     const currentPatient = apps[j].patientId;
@@ -19,20 +30,26 @@ function CalcFirstNextLastAppointment(apps, callback) {
     let count = 0;
     let i = j;
 
+    // loops through a subset of appointments
     while (i < apps.length && currentPatient === apps[i].patientId) {
       count += 1;
 
       const startDate = apps[i].startDate;
 
+      // sets the next appointment that is closest to today but in the future
       if (moment(startDate).isAfter(today)) {
         nextApptDate = apps[i].startDate;
         nextApptId = apps[i].id;
+
+      // sets first and last appointment to be equal if there is only one past appointment
       } else if (moment(startDate).isBefore(today) && count === 1) {
         lastApptDate = apps[i].startDate;
         lastApptId = apps[i].id;
 
         firstApptDate = apps[i].startDate;
         firstApptId = apps[i].id;
+
+      // set last appointment that is closest to today but in the past
       } else if (moment(startDate).isBefore(today) && !lastApptDate) {
         lastApptDate = apps[i].startDate;
         lastApptId = apps[i].id;
@@ -42,6 +59,11 @@ function CalcFirstNextLastAppointment(apps, callback) {
 
       i += 1;
     }
+
+    /**
+     * if there is more than one appointment in this subset check if the
+     * last appointment of this subset is in the past and then set first appointment
+     */
 
     if (count > 1 && moment(apps[i - 1].startDate).isBefore(today)) {
       firstApptId = apps[i - 1].id;
@@ -58,9 +80,11 @@ function CalcFirstNextLastAppointment(apps, callback) {
         lastApptDate,
       };
 
+    // update the current patient of this subset with the calculated data in the appointments object
       callback(currentPatient, appointmentsObj);
     }
 
+    // jump to the next subset of appointments if any
     j = i;
   }
 }
