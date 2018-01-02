76eeca93b0bbb60c3dd33976a9003ba56f68053d
Kirat <kirat_virk3@yahoo.ca>
2018-01-02 10:15:32 -0800

    Feature/1082/american codes (#1083)
    american codes migration

---
+++ server/migrations/20171227205408-american-dental-codes.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const procedures = require('../fixtures/procedures/procedureDumpV3American.json');
+
+module.exports = {
+  up: async function (queryInterface, Sequelize) {
+    return queryInterface.sequelize.transaction(async (t) => {
+      try {
+        await queryInterface.addColumn('Procedures', 'codeType', {
+          type: Sequelize.STRING,
+          allowNull: true,
+          defaultValue: 'Canadian Dental Code',
+        }, { transaction: t });
+
+        const syncProcedures = procedures.map((procedure) => {
+          return Object.assign({}, procedure, {
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          });
+        });
+
+        const code = procedures.map((procedure) => {
+          return procedure.code;
+        });
+
+        await queryInterface.changeColumn('Procedures', 'codeType', {
+          type: Sequelize.STRING,
+          allowNull: true,
+        }, { transaction: t });
+
+        await queryInterface.bulkDelete('Procedures', {
+          code,
+        }, { transaction: t });
+
+        await queryInterface.bulkUpdate('Procedures', {
+          codeType: 'American Dental Code',
+        }, {
+          code: {
+            $like: 'D%',
+          },
+        }, { transaction: t });
+
+        return queryInterface.bulkInsert(
+          'Procedures',
+          syncProcedures,
+          { transaction: t }
+        );
+      } catch (e) {
+        console.log(e);
+        t.rollback();
+      }
+    });
+  },
+
+  down: function (queryInterface) {
+    queryInterface.bulkDelete('DeliveredProcedures', null, {});
+    return queryInterface.bulkDelete('Procedures', null, {});
+  },
+};
