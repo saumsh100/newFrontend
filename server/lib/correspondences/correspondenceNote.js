45fee287ef9799223287a101ff47b8bea8ad8937
Justin Sharp <justin.d.sharp@gmail.com>
2018-02-12 21:53:46 -0700

    added tests for new correspondence generators

--- server/lib/correspondences/correspondenceNote.js
+++ server/lib/correspondences/correspondenceNote.js
@@ -1,6 +1,4 @@
 
-import moment from 'moment';
-
 const cap = str => str.replace(/\b\w/g, l => l.toUpperCase());
 const typeMap = {
   email: 'Email',
@@ -8,29 +6,53 @@ const typeMap = {
   phone: 'Phone',
 };
 
+/**
+ * reminderSent will generate the correspondance note for a REMINDER:SENT
+ *
+ * @param sentReminder
+ * @return {string}
+ */
 export function reminderSent(sentReminder) {
   const {
     interval,
     primaryType,
     isConfirmable,
   } = sentReminder;
-  const type = isConfirmable ? 'Confirmed' : 'Unconfirmed';
+  const type = isConfirmable ? 'Unconfirmed' : 'Confirmed';
   return `Sent "${cap(interval)} ${typeMap[primaryType]} ${type}" Reminder for Appointment via CareCru`;
 }
 
+/**
+ * reminderConfirmed will generate the correspondance note for a REMINDER:CONFIRMED
+ *
+ * @param sentReminder
+ * @return {string}
+ */
 export function reminderConfirmed(sentReminder) {
   const {
     interval,
     primaryType,
   } = sentReminder;
-  return `Patient Confirmed "${cap(interval)} ${typeMap[primaryType]} ${type}" Reminder for Appointment via CareCru`;
+  return `Patient Confirmed "${cap(interval)} ${typeMap[primaryType]}" Reminder for Appointment via CareCru`;
 }
 
+/**
+ * reminderConfirmed will generate the correspondance note for a RECALL:SENT
+ *
+ * @param sentRecall
+ * @return {string}
+ */
 export function recallSent(sentRecall) {
   const {
     interval,
     primaryType,
   } = sentRecall;
-  const time = interval.indexOf('-') > -1 ? 'After' : 'Before';
-  return `Sent "${cap(interval)} ${time} Due Date" ${typeMap[primaryType]} Recall via CareCru`;
+  let time = 'Before';
+  let prettyInterval = interval;
+  if (interval.indexOf('-') > -1) {
+    time = 'After';
+    prettyInterval = interval.slice(1, interval.length);
+  }
+
+  return `Sent "${cap(prettyInterval)} ${time} Due Date" ${typeMap[primaryType]} Recall via CareCru`;
 }
