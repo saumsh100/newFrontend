24fc5665c5b6fcaecb4434c9b34c2edbae38ad2e
Justin Sharp <justin.d.sharp@gmail.com>
2018-02-26 17:58:56 -0700

    ensuring no pictures are displayed

--- client/components/Widget/Review/Picture/index.jsx
+++ client/components/Widget/Review/Picture/index.jsx
@@ -4,19 +4,5 @@ import { Avatar } from '../../../library';
 import styles from './styles.scss';
 
 export default function Picture(props) {
-  const { reviewedPractitioner } = props;
-  if (!reviewedPractitioner.fullAvatarUrl) {
-    return <div className={styles.space} />;
-  }
-
-
-  return (
-    <div className={styles.blurBackground}>
-      <Avatar
-        size="md"
-        user={reviewedPractitioner}
-      />
-    </div>
-  );
+  return <div className={styles.space} />;
 }
-
