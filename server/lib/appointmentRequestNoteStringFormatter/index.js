de5ab7822e7642de0674e76ff38e3603e0d19a32
Ramon Villain <ramon.villain@gmail.com>
2018-06-27 14:01:43 -0700

    API-90 completed formatted note

--- server/lib/appointmentRequestNoteStringFormatter/index.js
+++ server/lib/appointmentRequestNoteStringFormatter/index.js
@@ -13,16 +13,18 @@ export function appointmentRequestNoteStringFormatter(request) {
     account,
     requestingPatientUser,
     insuranceCarrier,
+    insurerName,
     insuranceMemberId,
     insuranceGroupId,
   } = request;
 
   const nullHandler = (val, fallback = '') => val === null ? fallback : val;
+  const undefinedAndNullHandler = (val, fallback = '') => (val === undefined || val === null) ? fallback : val;
   const nameFormatter = ({ firstName, lastName }) => nullHandler(firstName) + ' ' + nullHandler(lastName);
   const birthDateHandler = (birthDate) => birthDate === null ? 'none' : moment(birthDate).format('DD/MM/YYYY');
 
   const practitionerName = practitioner === null ? 'No Preference' : nameFormatter(practitioner);
-  const requestNote =
+  const generalRequestNote =
     'Date Requested: ' +
     moment.tz(createdAt, account.timezone || 'America/Vancouver').format('h:mmA, DD/MM/YY') + '\r\n' +
     'Reason for Booking: ' + service.name + '\r\n' +
@@ -31,24 +33,27 @@ export function appointmentRequestNoteStringFormatter(request) {
     'Patient Name: ' + nameFormatter(patientUser) + '\r\n' +
     'Phone Number: ' + nullHandler((formatPhoneNumber(patientUser.phoneNumber)), 'none') + '\r\n' +
     'Email: ' + nullHandler(patientUser.email, 'none') + '\r\n' +
-    'Birth Date: ' + birthDateHandler(patientUser.birthDate) + '\r\n\r\n';
+    'Birth Date: ' + birthDateHandler(patientUser.birthDate) + '\r\n';
 
-  const sameRequestingPatientNote = 'Requested By: Self\r\n\r\n';
-  const differentRequestingPatientNote =
+  const payForMySelfNote = 'Insurance Plan: Pay for myself\r\n';
+  const insurancePlanNote = 'Insurance Plan: ' + insuranceCarrier + '\r\n' +
+    'Insurer Name: ' + undefinedAndNullHandler(insurerName, 'n/a') + '\r\n' +
+    'Member Id: ' + nullHandler(insuranceMemberId, 'none') + '\r\n' +
+    'Group Id: ' + nullHandler(insuranceGroupId, 'none');
+  const insuranceNote = (insuranceCarrier === null || insuranceCarrier === 'Pay for myself') ?
+    payForMySelfNote : insurancePlanNote;
+
+  const sameRequestedPatientNote = 'Requested By: Self';
+  const differentRequestedPatientNote =
     'Requested By: ' + nameFormatter(requestingPatientUser) + '\r\n' +
     'Phone Number: ' + nullHandler((formatPhoneNumber(requestingPatientUser.phoneNumber)), 'none') + '\r\n' +
     'Email: ' + nullHandler(requestingPatientUser.email, 'none') + '\r\n' +
-    'Birth Date: ' + birthDateHandler(requestingPatientUser.birthDate) + '\r\n\r\n';
-  const requestingPatientInfo = (patientUser.id === requestingPatientUser.id) ?
-    sameRequestingPatientNote : differentRequestingPatientNote;
+    'Birth Date: ' + birthDateHandler(requestingPatientUser.birthDate) + '\r\n';
+  const requestedPatientNote = (patientUser.id === requestingPatientUser.id) ?
+    insuranceNote + '\r\n\r\n' + sameRequestedPatientNote : '\r\n' + differentRequestedPatientNote + insuranceNote;
 
-  const payForMySelfNote = 'Insurance Plan: Pay for myself';
-  const insurancePlanNote = 'Insurance Plan: ' + insuranceCarrier + '\r\n' +
-    'Insurer Name: ' + null + '\r\n' +
-    'Member Id: ' + nullHandler(insuranceMemberId, 'none') + '\r\n' +
-    'Group Id: ' + nullHandler(insuranceGroupId, 'none');
+  const formattedNote = generalRequestNote + requestedPatientNote;
 
-  return (requestNote + requestingPatientInfo) +
-    ((insuranceCarrier === null || insuranceCarrier === 'Pay for myself') ?
-      payForMySelfNote : insurancePlanNote);
+  console.log(formattedNote);
+  return formattedNote;
 }
