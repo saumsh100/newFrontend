981663c0e0f381d3df56b53e80165f0a4bacdc07
Gavin Servai <gavinservai@gmail.com>
2018-10-25 10:12:32 -0700

    [2.23.1] API-238 Fixed Chat Migration (#1803)
    * API-238 Fixed the syntax of the update query, changed to using Promise, minor syntax upgrades.
    
    * Update package.json

--- server/migrations/20181022101607-add-hasUnread-boolean-in-chats.js
+++ server/migrations/20181022101607-add-hasUnread-boolean-in-chats.js
@@ -16,23 +16,25 @@ module.exports = {
 
         // MARK: - Update Chats
         const textMessages = await queryInterface.sequelize.query('SELECT "chatId" FROM "TextMessages" WHERE "read" = false GROUP BY "chatId"');
-        
-        textMessages[0].forEach(async ({ chatId }) => {
-          await queryInterface.sequelize.query(
+        if (textMessages[0].length > 0) {
+          Promise.all(textMessages[0].map(({ chatId }) => queryInterface.sequelize.query(
             `UPDATE "Chats" 
-            SET "hasUnread" = true
-            WHERE id = ${chatId}`,
+              SET "hasUnread" = true
+              WHERE id = '${chatId}'`,
             { transaction },
-          );
-        });
+          ))).catch((err) => {
+            console.error(err);
+            transaction.rollback();
+          });
+        }
       } catch (err) {
         console.error(err);
         transaction.rollback();
       }
     });
   },
-  down: (queryInterface) => {
-    return queryInterface.sequelize.transaction(async (t) => {
+  down: queryInterface =>
+    queryInterface.sequelize.transaction(async (t) => {
       try {
         await queryInterface.removeColumn(
           'Chats',
@@ -43,6 +45,5 @@ module.exports = {
         console.error(err);
         t.rollback();
       }
-    });
-  },
+    }),
 };
