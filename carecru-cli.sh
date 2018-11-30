a0102d6a7d93735c741c9313b380a267b508af75
Alexandre Ferreira <alexandrefs.ferreira@gmail.com>
2018-11-30 15:32:04 -0200

    DEV-58 fixing commands to start in compose and update carecru-cli (#1862)

--- carecru-cli.sh
+++ carecru-cli.sh
@@ -6,9 +6,8 @@ initialize() {
 	redoSeeds
 	serverWatch
 	clientDevServer
+	[ $? != 0 ] && echo "Failed to Initialize Development Environment" && exit 1
 	echo "Initialization Complete"
-
-	[ $? != 0 ] && error "Failed to Initialize Development Environment" && exit 101
 }
 
 start() {
@@ -16,23 +15,20 @@ start() {
 	dockerComposeUp
 	serverWatch
 	clientDevServer
+	[ $? != 0 ] && echo "Failed to Start-up Development Environment" && exit 1
 	echo "Start-up Complete"
-
-	[ $? != 0 ] && error "Failed to Start-up Development Environment" && exit 101
 }
 
 stop() {
 	echo "Killing Development Environment"
 	docker-compose kill
-
-	[ $? != 0 ] && error "Failed to Kill Development Environment" && exit 101
+	[ $? != 0 ] && echo "Failed to Kill Development Environment" && exit 1
 }
 
 console() {
 	echo "Opening Bash Console to carecru_web"
 	docker exec -it $(docker ps -q --filter ancestor=carecru_web:development) bash
-
-	[ $? != 0 ] && error "Failed to Open Bash Console to carecru_web" && exit 101
+	[ $? != 0 ] && echo "Failed to Open Bash Console to carecru_web" && exit 1
 }
 
 log() {
@@ -43,54 +39,55 @@ log() {
 dockerComposeUp() {
 	echo "Starting up Docker Containers"
 	docker-compose up -d
-
-	[ $? != 0 ] && error "Failed to Run docker-compose up" && exit 101
+	[ $? != 0 ] && echo "Failed to Run docker-compose up" && exit 1
 }
 
 serverWatch() {
 	echo "Opening Server Watch"
-	open watch_server.command
-
-	[ $? != 0 ] && error "Failed to Open Server Watch" && exit 101
+	if [[ $(uname -a) == *"Linux"* ]]; then
+		gnome-terminal -- sh -c "docker exec -it $(docker ps -q --filter ancestor=carecru_web:development) npm run server:watch; read"
+		[ $? != 0 ] && echo "Failed to Open Server Watch" && exit 1
+	elif [[ $(uname -a) == *"Darwin"* ]]; then
+		open watch_server.command
+		[ $? != 0 ] && echo "Failed to Open Server Watch" && exit 1
+	fi
 }
 
 clientDevServer() {
 	echo "Opening Client Dev Server"
-	open client_dev_server.command
-
-	[ $? != 0 ] && error "Failed to Open Client Dev Server" && exit 101
+	if [[ $(uname -a) == *"Linux"* ]]; then
+		gnome-terminal -- sh -c "docker exec -it $(docker ps -q --filter ancestor=carecru_web:development) npm run client:dev:server; read"
+		[ $? != 0 ] && echo "Failed to Open Client Dev Server" && exit 1
+	elif [[ $(uname -a) == *"Darwin"* ]]; then
+		open client_dev_server.command
+		[ $? != 0 ] && echo "Failed to Open Client Dev Server" && exit 1
+	fi
 }
 
 redoSeeds() {
 	echo "Running Seeds and Migrations"
 	docker exec -it $(docker ps -q --filter ancestor=carecru_web:development) sequelize db:migrate:undo:all
-
 	docker exec -it $(docker ps -q --filter ancestor=carecru_web:development) sequelize db:migrate
-
 	docker exec -it $(docker ps -q --filter ancestor=carecru_web:development) sequelize db:seed:all
-
-	[ $? != 0 ] && error "Failed to Run Seeds and Migrations" && exit 101
+	[ $? != 0 ] && echo "Failed to Run Seeds and Migrations" && exit 1
 }
 
 createMigration() {
 	echo "Creating Migration $1"
 	docker exec -it $(docker ps -q --filter ancestor=carecru_web:development) sequelize migration:create --name $1
-
-	[ $? != 0 ] && error "Failed to Create Migration $1" && exit 101
+	[ $? != 0 ] && echo "Failed to Create Migration $1" && exit 1
 }
 
 runMigrations() {
 	echo "Running Migrations"
 	docker exec -it $(docker ps -q --filter ancestor=carecru_web:development) sequelize db:migrate
-
-	[ $? != 0 ] && error "Failed to Run Migrations" && exit 101
+	[ $? != 0 ] && echo "Failed to Run Migrations" && exit 1
 }
 
 undoMigrations() {
 	echo "Undoing Migrations"
 	docker exec -it $(docker ps -q --filter ancestor=carecru_web:development) sequelize db:migrate:undo
-
-	[ $? != 0 ] && error "Failed to Undo Migrations" && exit 101
+	[ $? != 0 ] && echo "Failed to Undo Migrations" && exit 1
 }
 
 $*
