1934fdb2562daf7da94bfdd3420fc0cdbe69fc88
Lucas Taliberti de Sousa <lucastaliberti@gmail.com>
2019-02-05 16:30:03 -0800

    API-337 GraphQL endpoint that proxies the requests to the new API (#1985)
    https://carecru.atlassian.net/browse/API-337
    
    A simple endpoint that uses the GraphQL to proxy the requests from the legacy API to the new one. Querying works the same way as I would from a regular server.
    
    - naming of the endpoint is subject to change, please add your suggestions as comments here.
    
    + formating of the file and removal of unused variables.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 
 import { Router } from 'express';
 import subdomain from 'express-subdomain';
-import url from 'url';
+import graphQLRouter from 'CareCruGraphQL/server';
 import sequelizeApiRouter from './_api';
 import sequelizeAuthRouter from './_auth';
 import callsRouterSequelize from './_callrail';
@@ -12,16 +12,12 @@ import signupRouterSequelize from './_signup';
 import vendastaRouterSequelize from './_vendasta';
 import resetRouter from './reset';
 import {
-  Account,
-  Appointment,
   Invite,
-  Patient,
-  Token,
   PasswordReset,
   User,
 } from '../_models';
-import { renderFamilyRemindersHTML } from '../lib/emailTemplates/familyReminders';
-import graphQLRouter from 'CareCruGraphQL/server';
+import graphQLClient from '../util/graphQLClient';
+import { sequelizeAuthMiddleware } from '../middleware/auth';
 
 const rootRouter = Router();
 
@@ -45,6 +41,15 @@ rootRouter.use('/api', sequelizeApiRouter);
 
 // Bind GraphQL endpoint
 rootRouter.use('/graphql', graphQLRouter);
+rootRouter.post('/newgraphql', sequelizeAuthMiddleware, async (req, res, next) => {
+  try {
+    const { data } = await graphQLClient(req.body);
+
+    res.send(data);
+  } catch ({ data }) {
+    res.status(400).send(data);
+  }
+});
 
 // Webhooks!
 rootRouter.use('/twilio', twilioRouterSequelize);
@@ -53,32 +58,34 @@ rootRouter.use('/callrail', callsRouterSequelize);
 rootRouter.use('/_callrail', callsRouterSequelize);
 rootRouter.use('/_vendasta', vendastaRouterSequelize);
 
-rootRouter.get('/signupinvite/:tokenId', (req, res, next) => Invite.findOne({
-  where: { token: req.params.tokenId },
-  paranoid: false,
-})
-  .then((invite) => {
-    if (!invite) {
-      // TODO: replace with StatusError
-      return res.status(404).send('404 NOT FOUND');
-    } else if (invite && invite.deletedAt) {
-      return res.status(404).send('Invite Expired/Cancelled');
-    }
-    return res.redirect(`/signup/${req.params.tokenId}`);
+rootRouter.get('/signupinvite/:tokenId', (req, res, next) =>
+  Invite.findOne({
+    where: { token: req.params.tokenId },
+    paranoid: false,
   })
-  .catch(next));
+    .then((invite) => {
+      if (!invite) {
+        // TODO: replace with StatusError
+        return res.status(404).send('404 NOT FOUND');
+      } else if (invite && invite.deletedAt) {
+        return res.status(404).send('Invite Expired/Cancelled');
+      }
+      return res.redirect(`/signup/${req.params.tokenId}`);
+    })
+    .catch(next));
 
 // below route is sequelize
-rootRouter.get('/reset/:tokenId', (req, res, next) => PasswordReset.findOne({ where: { token: req.params.tokenId } })
-  .then((reset) => {
-    if (!reset) {
-      // TODO: replace with StatusError
-      res.status(404).send();
-    } else {
-      res.redirect(`/resetpassword/${req.params.tokenId}`);
-    }
-  })
-  .catch(next));
+rootRouter.get('/reset/:tokenId', (req, res, next) =>
+  PasswordReset.findOne({ where: { token: req.params.tokenId } })
+    .then((reset) => {
+      if (!reset) {
+        // TODO: replace with StatusError
+        res.status(404).send();
+      } else {
+        res.redirect(`/resetpassword/${req.params.tokenId}`);
+      }
+    })
+    .catch(next));
 
 rootRouter.post('/userCheck', (req, res, next) => {
   const username = req.body.email.toLowerCase().trim();
