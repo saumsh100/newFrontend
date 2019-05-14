1c35b847e62d43c010dabc364d14406ffa89a46d
TajVeerKaurThiara <46328709+TajVeerKaurThiara@users.noreply.github.com>
2019-05-14 12:34:16 -0700

    API-515 Endpoint to Confirm Appointments via Robo-calls (#2101)
    * API-515 Endpoint to Confirm Appointments via Robo-calls
    
    * API-515 adding logic for specific say and/or dial for if the appointments are/ are not confirmed
    
    * API-515 removing unused endpoint

--- server/routes/_twilio/voice/index.js
+++ server/routes/_twilio/voice/index.js
@@ -2,89 +2,87 @@
 import { Router } from 'express';
 import Twilio from 'twilio';
 import {
+  Account,
   Appointment,
   Reminder,
-  SentReminder,
-} from '../../../_models';
+  Patient,
+  SentRemindersPatients,
+} from 'CareCruModels';
+import { sequelizeLoader } from '../../util/loaders';
 
 const voiceRouter = Router();
 
-const actionMessage = () => 'Press one now to confirm that you can make your appointment. To ' +
-  'cancel or reschedule, hang up now and call us. To play back ' +
-  'this message, press nine.';
+voiceRouter.param(
+  'sentReminderId',
+  sequelizeLoader('sentReminder', 'SentReminder', [
+    {
+      model: Patient,
+      as: 'patient',
+    },
+    {
+      model: Reminder,
+      as: 'reminder',
+    },
+    {
+      model: Account,
+      as: 'account',
+    },
+    {
+      model: SentRemindersPatients,
+      as: 'sentRemindersPatients',
+      include: [
+        {
+          model: Appointment,
+          as: 'appointment',
+        },
+        {
+          model: Patient,
+          as: 'patient',
+        },
+      ],
+    },
+  ]),
+);
 
-const createReminderMessage = ({ firstName, clinicName, startDate, startTime }) => `Hello ${firstName}, this is a friendly reminder from ${clinicName} ` +
-  `that you have an appointment on ${startDate} at ${startTime}. ${actionMessage()}`;
+voiceRouter.get(
+  '/sentReminders/:sentReminderId/confirm',
+  async ({ sentReminder }, res, next) => {
+    res.type('text/xml');
 
+    const { VoiceResponse } = Twilio.twiml;
+    const twiml = new VoiceResponse();
 
-// Handle all automated Call interaction with twilio numbers
-voiceRouter.post('/sentReminders/:sentReminderId', async (req, res, next) => {
-  const sentReminder = await SentReminder.findOne({
-    where: { id: req.params.sentReminderId },
-    include: [
-      {
-        model: Appointment,
-        as: 'appointment',
-      },
-      {
-        model: Reminder,
-        as: 'reminder',
-      },
-    ],
-    raw: true,
-    nest: true,
-  });
+    try {
+      await sentReminder.update({ isConfirmed: true });
 
-  if (!sentReminder) {
-    return res.sendStatus(404);
-  }
-  const { appointment, reminder } = sentReminder;
+      // For any confirmed reminder we confirm appointment
+      const { reminder, account, sentRemindersPatients } = sentReminder;
 
-  // Use the Twilio Node.js SDK to build an XML response
-  const { VoiceResponse } = Twilio.twiml;
-  const twiml = new VoiceResponse();
+      const appointmentString =
+        sentRemindersPatients.length === 1 ? 'appointment' : 'appointments';
+      const pluralOrNot =
+        sentRemindersPatients.length === 1
+          ? 'Your appointment was'
+          : 'Your appointments were';
 
-  const { Digits } = req.body;
-  const {
-    firstName,
-    clinicName,
-    startDate,
-    startTime,
-  } = req.query;
+      try {
+        await Promise.all(
+          sentRemindersPatients.map(({ appointment }) =>
+            appointment.confirm(reminder)),
+        );
+        twiml.say(`${pluralOrNot} confirmed. Thank you. Goodbye.`);
+      } catch (e) {
+        twiml.say(
+          `Contacting the office as we were unable to confirm the ${appointmentString}`,
+        );
+        twiml.dial(account.phoneNumber);
+      }
 
-  if (!Digits || Digits === '9') {
-    const gather = twiml.gather({
-      timeout: 45,
-      numDigits: 1,
-    });
-    const text = createReminderMessage({
-      firstName,
-      clinicName,
-      startDate,
-      startTime,
-    });
-
-    gather.say({ voice: 'alice' }, text);
-    twiml.redirect({ method: 'POST' }, req.originalUrl);
-  } else if (Digits === '1') {
-    twiml.say({ voice: 'alice' }, 'Thank you for confirming your appointment. Goodbye.');
-    twiml.hangup();
-
-    // Confirm the appointment if any one sentReminder is confirmed
-    await sentReminder.update({ isConfirmed: true });
-    await appointment.confirm(reminder);
-  } else {
-    const gather = twiml.gather({
-      timeout: 30,
-      numDigits: 1,
-    });
-
-    gather.say({ voice: 'alice' }, `I'm sorry, that was not a valid response. ${actionMessage()}`);
-    twiml.redirect({ method: 'POST' }, '/twilio/voice/reminders');
-  }
-
-  res.type('text/xml');
-  res.send(twiml.toString());
-});
+      res.send(twiml.toString());
+    } catch (err) {
+      next(err);
+    }
+  },
+);
 
 module.exports = voiceRouter;
