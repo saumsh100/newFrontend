4492037df731843d2155d40866250743bbf89077
CalebDSPark <caleb.park@carecru.com>
2019-10-04 17:18:30 -0700

    DAS-714 Add ability to scroll to recording audio (#57)
    * DAS-714 Add ability to scroll to reording audio
    
    * DAS-714 Remove unnecessary comments
    
    * DAS-714 Remove unnecessary function call

--- client/components/Calls/CallInfo/index.jsx
+++ client/components/Calls/CallInfo/index.jsx
@@ -5,22 +5,8 @@ import GoogleMapReact from 'google-map-react';
 import PropTypes from 'prop-types';
 import { formatPhoneNumber } from '@carecru/isomorphic';
 import InfoDump from '../../Patients/Shared/InfoDump';
-import { Form, Field } from '../../library';
-import styles from './styles.scss';
 import { callShape } from '../../library/PropTypeShapes';
-
-const Marker = ({ text }) => <div className={styles.marker}>{text}</div>;
-
-const AppBookedForm = ({ initialValues, id, handleToggle }) => (
-  <Form
-    form={`appBookedForm_${id}`}
-    onChange={handleToggle}
-    initialValues={initialValues}
-    ignoreSaveButton
-  >
-    <Field name="wasApptBooked" component="Toggle" />
-  </Form>
-);
+import styles from './styles.scss';
 
 class CallInfo extends Component {
   constructor(props) {
@@ -30,9 +16,6 @@ class CallInfo extends Component {
       lng: null,
       loaded: false,
     };
-    this.renderLeftPanel = this.renderLeftPanel.bind(this);
-    this.renderRightPanel = this.renderRightPanel.bind(this);
-    this.handleToggle = this.handleToggle.bind(this);
   }
 
   componentDidMount() {
@@ -58,98 +41,61 @@ class CallInfo extends Component {
       );
   }
 
-  handleToggle(values) {
+  render() {
     const { call } = this.props;
-
-    return this.props.handleCallUpdate(call.id, values.wasApptBooked);
-  }
-
-  renderLeftPanel() {
-    const { call } = this.props;
-
     const callerId =
       call.callerCity && call.callerState ? `${call.callerCity}, ${call.callerState}` : null;
 
     return (
-      <div className={styles.leftPanel}>
-        <div className={styles.title}>Caller Info</div>
-        <div className={styles.row}>
-          <InfoDump label="Name" data={call.callerName} />
-          <InfoDump label="Initial Source" data={call.callSource} />
-        </div>
-        <div className={styles.row}>
-          <InfoDump label="Received" data={moment(call.startTime).format('MMM DD, YYYY h:mm A')} />
-          <InfoDump label="Phone number" data={formatPhoneNumber(call.callerNum)} />
-        </div>
-        <div className={styles.row}>
-          <InfoDump label="Total calls" data={call.totalCalls} />
-          <InfoDump label="Caller ID" data={callerId} />
-        </div>
-        {this.state.loaded && (
-          <div className={styles.map}>
-            <GoogleMapReact
-              bootstrapURLKeys={{ key: process.env.GOOGLE_API_KEY }}
-              defaultCenter={{
-                lat: this.state.lat,
-                lng: this.state.lng,
-              }}
-              defaultZoom={6}
-            />
-          </div>
-        )}
-      </div>
-    );
-  }
-
-  renderRightPanel() {
-    const { call } = this.props;
-
-    return (
-      <div className={styles.rightPanel}>
-        <div className={styles.title}>Recording</div>
-        {call.recording ? (
-          <div className={styles.playerWrapper}>
-            <audio controls controlsList="nodownload">
-              <source src={call.recording} type="audio/ogg" />
-              <track kind="captions" />
-            </audio>
-          </div>
-        ) : (
-          <div className={styles.noData}> N/A </div>
-        )}
-      </div>
-    );
-  }
-
-  render() {
-    return (
       <div className={styles.callInfoModal}>
-        {this.renderLeftPanel()}
-        {this.renderRightPanel()}
+        <div className={styles.leftPanel}>
+          <div className={styles.title}>Recording</div>
+          {call.recording ? (
+            <div className={styles.playerWrapper}>
+              <audio controls controlsList="nodownload">
+                <source src={call.recording} type="audio/ogg" />
+                <track kind="captions" />
+              </audio>
+            </div>
+          ) : (
+            <div className={styles.noData}> N/A </div>
+          )}
+          <div className={styles.title}>Caller Info</div>
+          <div className={styles.row}>
+            <InfoDump label="Name" data={call.callerName} />
+            <InfoDump label="Initial Source" data={call.callSource} />
+          </div>
+          <div className={styles.row}>
+            <InfoDump
+              label="Received"
+              data={moment(call.startTime).format('MMM DD, YYYY h:mm A')}
+            />
+            <InfoDump label="Phone number" data={formatPhoneNumber(call.callerNum)} />
+          </div>
+          <div className={styles.row}>
+            <InfoDump label="Total calls" data={call.totalCalls} />
+            <InfoDump label="Caller ID" data={callerId} />
+          </div>
+          {this.state.loaded && (
+            <div className={styles.map}>
+              <GoogleMapReact
+                bootstrapURLKeys={{ key: process.env.GOOGLE_API_KEY }}
+                defaultCenter={{
+                  lat: this.state.lat,
+                  lng: this.state.lng,
+                }}
+                defaultZoom={6}
+              />
+            </div>
+          )}
+        </div>
       </div>
     );
   }
 }
 
-AppBookedForm.propTypes = {
-  id: PropTypes.string,
-  handleToggle: PropTypes.func.isRequired,
-  initialValues: PropTypes.objectOf(PropTypes.bool).isRequired,
-};
-
-AppBookedForm.defaultProps = {
-  id: null,
-};
-
-Marker.propTypes = { text: PropTypes.string };
-
-Marker.defaultProps = {
-  text: null,
-};
-
 CallInfo.propTypes = {
   call: PropTypes.shape(callShape).isRequired,
-  handleCallUpdate: PropTypes.func.isRequired,
 };
 
 export default CallInfo;
