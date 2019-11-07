4347654d58537bb1fbba7a2f7d234a7ad4600c3c
Ramon Villain <ramon.villain@gmail.com>
2019-11-06 19:22:54 -0800

    CCRU-991 Update Follow Up Preview component (#87)

--- client/components/Settings/Donna/Recalls/RecallPreview/index.jsx
+++ client/components/Settings/Donna/Recalls/RecallPreview/index.jsx
@@ -1,88 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Header, SBody, SContainer, SHeader} from '../../../../library';
+import EmailPreview from '../../../Shared/EmailPreview';
+import SmsPreview from '../../../Shared/SmsPreview';
+import CommsPreview, {CommsPreviewSection} from '../../../Shared/CommsPreview';
+import styles from './styles.scss';
 
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { Header, SContainer, SHeader, SBody } from "../../../../library";
-import EmailPreview from "../../../Shared/EmailPreview";
-import SmsPreview from "../../../Shared/SmsPreview";
-import CommsPreview, {
-  CommsPreviewSection
-} from "../../../Shared/CommsPreview";
-import styles from "./styles.scss";
-
-function RecallSMSPreview({ account, recall }) {
-  return (
+const RecallSMSPreview = ({ account, recall }) => (
     <div className={styles.smsPreviewWrapper}>
-      <SmsPreview
-        account={account}
-        url={`/api/accounts/${account.id}/recalls/${recall.id}/sms`}
-      />
+        <SmsPreview account={account} url={`/api/accounts/${account.id}/recalls/${recall.id}/sms`}/>
     </div>
-  );
-}
+);
 
-class RecallPreview extends Component {
-  constructor(props) {
-    super(props);
-  }
+const RecallPreview = ({recall, account}) => {
 
-  /* componentWillReceiveProps(nextProps) {
-
-  } */
-
-  render() {
-    const { recall, account } = this.props;
-
-    const { primaryTypes } = recall;
-
+    const {primaryTypes} = recall;
     // Slice so that it's immutable, reverse so that SMS is first cause its a smaller component
     const commsPreviewSections = primaryTypes
-      .slice()
-      .reverse()
-      .map((type) => {
-        let typePreview = null;
-        if (type === 'sms') {
-          typePreview = (
-            <div>
-              <RecallSMSPreview recall={recall} account={account} />
-            </div>
-          );
-        } else if (type === 'email') {
-          const url = `/api/accounts/${account.id}/recalls/${
-            recall.id
-          }/preview`;
-          typePreview = (
-            <div>
-              <EmailPreview url={url} />
-            </div>
-          );
-        } else if (type === 'phone') {
-          typePreview = (
-            <div className={styles.smsPreviewWrapper}>Phone Preview</div>
-          );
-        }
+        .slice()
+        .reverse()
+        .map((type) => {
+            let typePreview = null;
+            if (type === 'sms' || type === 'smart_follow_up') {
+                typePreview = (
+                    <div>
+                        <RecallSMSPreview recall={recall} account={account}/>
+                    </div>
+                );
+            } else if (type === 'email') {
+                const url = `/api/accounts/${account.id}/recalls/${recall.id}/preview`;
+                typePreview = (
+                    <div>
+                        <EmailPreview url={url}/>
+                    </div>
+                );
+            } else if (type === 'phone') {
+                typePreview = <div className={styles.smsPreviewWrapper}>Phone Preview</div>;
+            }
 
-        return (
-          <CommsPreviewSection key={`${recall.id}_${type}`}>
-            {typePreview}
-          </CommsPreviewSection>
-        );
-      });
+            return (
+                <CommsPreviewSection key={`${recall.id}_${type}`}>{typePreview}</CommsPreviewSection>
+            );
+        });
 
     return (
-      <SContainer>
-        <SHeader className={styles.previewHeader}>
-          <div className={styles.topHeader}>
-            <Header title="Preview" />
-          </div>
-        </SHeader>
-        <SBody className={styles.previewSBody}>
-          {/* TODO: No need for Tabs here, just need to be able to determine type and isConfirmable */}
-          <CommsPreview>{commsPreviewSections}</CommsPreview>
-        </SBody>
-      </SContainer>
+        <SContainer>
+            <SHeader className={styles.previewHeader}>
+                <div className={styles.topHeader}>
+                    <Header title="Preview"/>
+                </div>
+            </SHeader>
+            <SBody className={styles.previewSBody}>
+                <CommsPreview>{commsPreviewSections}</CommsPreview>
+            </SBody>
+        </SContainer>
     );
-  }
-}
+};
 
 RecallPreview.propTypes = {
   recall: PropTypes.object.isRequired,
