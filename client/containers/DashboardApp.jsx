8fc9db50065480ad790ebe0a8e271634776fc59b
Justin Sharp <justin@carecru.com>
2020-09-03 09:34:14 -0600

    set pooling to vwr and unread chats (#235)
    * [WIP] set pooling to vwr and unread chat
    
    * PR suggestions
    
    * fixing request time and setting intervals
    
    * set the interval to use seconds
    
    Co-authored-by: Ricardo Georgel <rickygeorgel@yahoo.com.br>

--- client/containers/DashboardApp.jsx
+++ client/containers/DashboardApp.jsx
@@ -1,7 +1,8 @@
 
 import PropTypes from 'prop-types';
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import TopBarContainer from '../containers/TopBarContainer';
 import NavRegionContainer from '../containers/NavRegionContainer';
 import MainRegionContainer from '../containers/MainRegionContainer';
@@ -13,40 +14,136 @@ import AlertContainer from '../containers/AlertContainer';
 import PatientActionsContainer from '../components/Patients/Shared/PatientActionsContainer';
 import { locationShape } from '../components/library/PropTypeShapes/routerShapes';
 import styles from './styles.scss';
+import { isFeatureEnabledSelector } from '../reducers/featureFlags';
+import { fetchWaitingRoomQueue } from '../thunks/waitingRoom';
+import { loadUnreadChatCount } from '../thunks/chat';
 
-const DashboardApp = ({ location, children }) => {
-  if (location.pathname.includes('login')) return <div>{children}</div>;
+class DashboardApp extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const isCollapsed = useSelector(({ toolbar }) => toolbar.get('isCollapsed'));
-  const isSearchCollapsed = useSelector(({ toolbar }) => toolbar.get('isSearchCollapsed'));
-  return (
-    <div>
-      <CallerModal />
-      <TopBarContainer />
-      {!isCollapsed && (
-        <div className={styles.overlay} onClick={() => setIsCollapsed(!isCollapsed)} />
-      )}
-      <NavRegionContainer>
-        <NavList location={location} isCollapsed={isCollapsed} />
-      </NavRegionContainer>
-      <MainRegionContainer>
-        {isSearchCollapsed && (
-          <div className={styles.subTabs}>
-            <SubTabs location={location} />
-          </div>
+    this.unreadChatIntervalId = null;
+    this.vwrIntervalId = null;
+  }
+
+  componentDidUpdate() {
+    const { isPollingNeeded } = this.props;
+
+    if (isPollingNeeded) {
+      this.startPollingUnreadChats();
+      this.startPollingWaitingRoom();
+    } else {
+      this.resetIntervals();
+    }
+  }
+
+  componentWillUnmount() {
+    this.resetIntervals();
+  }
+
+  initiatePolling({ interval, pollFn }) {
+    return setInterval(() => pollFn(), interval * 1000);
+  }
+
+  startPollingUnreadChats() {
+    if (!this.unreadChatIntervalId) {
+      this.unreadChatIntervalId = this.initiatePolling({
+        interval: Number(process.env.POLLING_UNREAD_CHAT_INTERVAL || '300'),
+        pollFn: () => this.props.loadUnreadChatCount(),
+      });
+    }
+  }
+
+  startPollingWaitingRoom() {
+    if (!this.vwrIntervalId) {
+      this.vwrIntervalId = this.initiatePolling({
+        interval: Number(process.env.POLLING_VWR_INTERVAL || '60'),
+        pollFn: () => this.props.fetchWaitingRoomQueue({ accountId: this.props.accountId }),
+      });
+    }
+  }
+
+  resetIntervals() {
+    clearInterval(this.unreadChatIntervalId);
+    clearInterval(this.vwrIntervalId);
+
+    this.unreadChatIntervalId = null;
+    this.vwrIntervalId = null;
+  }
+
+  render() {
+    const { location, children } = this.props;
+
+    if (location.pathname.includes('login')) return <div>{children}</div>;
+
+    const { isCollapsed, isSearchCollapsed } = this.props;
+
+    return (
+      <div>
+        <CallerModal />
+        <TopBarContainer />
+        {!isCollapsed && (
+          // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+          <div
+            className={styles.overlay}
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            role="button"
+            tabIndex={0}
+          />
         )}
-        <div className={styles.mainRegionChildren}>
-          {children}
-          <AlertContainer />
-          <PatientActionsContainer />
-        </div>
-      </MainRegionContainer>
-    </div>
+        <NavRegionContainer>
+          <NavList location={location} isCollapsed={isCollapsed} />
+        </NavRegionContainer>
+        <MainRegionContainer>
+          {isSearchCollapsed && (
+            <div className={styles.subTabs}>
+              <SubTabs location={location} />
+            </div>
+          )}
+          <div className={styles.mainRegionChildren}>
+            {children}
+            <AlertContainer />
+            <PatientActionsContainer />
+          </div>
+        </MainRegionContainer>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ featureFlags, toolbar, auth }) {
+  const isPollingNeeded = isFeatureEnabledSelector(featureFlags.get('flags'), 'is-polling-needed');
+
+  return {
+    isPollingNeeded,
+    isCollapsed: toolbar.get('isCollapsed'),
+    isSearchCollapsed: toolbar.get('isSearchCollapsed'),
+    accountId: auth.get('accountId'),
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      fetchWaitingRoomQueue,
+      loadUnreadChatCount,
+    },
+    dispatch,
   );
-};
+}
 
 DashboardApp.propTypes = {
   children: PropTypes.node.isRequired,
   location: PropTypes.shape(locationShape).isRequired,
+  isCollapsed: PropTypes.PropTypes.bool.isRequired,
+  isSearchCollapsed: PropTypes.bool.isRequired,
+  isPollingNeeded: PropTypes.bool.isRequired,
+  accountId: PropTypes.string.isRequired,
+  fetchWaitingRoomQueue: PropTypes.func.isRequired,
+  loadUnreadChatCount: PropTypes.func.isRequired,
 };
-export default DashboardApp;
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(DashboardApp);
