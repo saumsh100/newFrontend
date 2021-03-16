b2c5619738d513ddf49c34f19e7562b7e7fbb4e9
Ömer Yalhı <57917316+oyalhi@users.noreply.github.com>
2021-03-16 10:19:56 -0700

    [incorrect-dashboard-url] implemented route to be redirected in the router only (#421)
    Co-authored-by: Omer Yalhi <omer@carecru.com>

--- client/socket/connectSocketToStoreLogin.js
+++ client/socket/connectSocketToStoreLogin.js
@@ -26,7 +26,8 @@ export default function connectSocketToStoreLogin(store, socket) {
         DesktopNotification.showNotification('Patient has entered waiting room', {
           body: `${patient.firstName} ${patient.lastName} has entered the virtual waiting room.`,
           onClick: () => {
-            dispatch(push('/'));
+            // this is intentionally not fixed, it will redirect in the router
+            dispatch(push('/dashboard'));
           },
         });
 
