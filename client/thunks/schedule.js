04aa67e2e97e15683c43dba4b96d678a095b345f
leo yu <leo.yu@carecru.com>
2021-03-26 16:01:28 -0600

    CBUG-1598 Partial Matches not being detected (#426)
    * CBUG-1598 try search if no exact match for connected patient
    
    * Hotfix-url-and-proxy-for-local-dev-with-local-backend
    
    * Update webpack config
    
    * socket issue
    
    * Update webpack.config.ccp.js
    
    * match info no matter is connected or not
    
    Co-authored-by: Paulo Andrade <paulo85br@gmail.com>

--- client/thunks/schedule.js
+++ client/thunks/schedule.js
@@ -1,6 +1,4 @@
-
 import set from 'lodash/set';
-import values from 'lodash/values';
 import each from 'lodash/each';
 import size from 'lodash/size';
 import filter from 'lodash/filter';
@@ -12,79 +10,11 @@ import { updateEntityRequest } from './fetchEntities';
 import { getUTCDate, nonApptWritePMS } from '../components/library';
 
 export function checkPatientUser(patientUser, requestData) {
-  return async function (dispatch, getState) {
+  return async function(dispatch, getState) {
     const { auth } = getState();
     const apptWrite = !nonApptWritePMS(auth.get('adapterType'));
-
-    // AccountId passed up in the query
-    const query = { patientUserId: patientUser.get('id') };
-
-    try {
-      const patientData = await httpClient().get('/api/patients', { params: query });
-
-      const { data } = patientData;
-      const checkObjEmpty = !values(data.entities.patients).some(x => x !== undefined);
-
-      if (!checkObjEmpty) {
-        return connectedPatientUser(dispatch, requestData, data, patientUser);
-      }
-
-      return suggestedPatients(dispatch, requestData, patientUser, apptWrite);
-    } catch (err) {
-      throw err;
-    }
+    suggestedPatients(dispatch, requestData, patientUser, apptWrite);
   };
-}
-
-async function connectedPatientUser(dispatch, requestData, data, patientUser) {
-  const patient = Object.values(data.entities.patients)[0];
-  const patientId = patient.id;
-  dispatch(
-    receiveEntities({
-      key: 'patients',
-      entities: data.entities,
-    }),
-  );
-
-  const query = { requestCreatedAt: requestData.createdAt };
-
-  const modifiedRequest = Object.assign({}, requestData);
-  set(modifiedRequest, 'patientId', patientId);
-
-  const appInfo = await httpClient().get(`/api/patients/${patientId}/nextAppointment`, {
-    params: query,
-  });
-  const params = suggestionParams(patientUser, requestData);
-
-  if (appInfo.data.entities.appointments) {
-    const appList = getEntities(appInfo.data.entities);
-    const exactMatch = [];
-    const partialMatch = [];
-
-    appList.forEach((appointment) => {
-      matchPatient(patient, params, appointment, requestData, exactMatch, partialMatch);
-    });
-
-    if (size(exactMatch)) {
-      const selectedApp = exactMatch[0];
-      const name = `${patient.firstName} ${patient.lastName}`;
-      dispatch(
-        updateEntityRequest({
-          url: `/api/requests/${requestData.requestId}/confirm/${selectedApp.id}`,
-          values: {},
-          alert: alertRequestUpdate(name),
-        }),
-      );
-      return data.entities;
-    }
-    if (size(partialMatch)) {
-      set(modifiedRequest, 'nextAppt', appList);
-      dispatch(selectAppointment(modifiedRequest));
-    }
-  }
-
-  dispatch(selectAppointment(modifiedRequest));
-  return data.entities;
 }
 
 async function suggestedPatients(dispatch, requestData, patientUser, apptWrite) {
@@ -98,11 +28,11 @@ async function suggestedPatients(dispatch, requestData, patientUser, apptWrite) 
     }),
   );
 
-  const cloneRequestData = Object.assign({}, requestData);
+  const cloneRequestData = { ...requestData };
 
   const patients = getEntities(dataSuggest.entities);
-  const patientsWithAppts = filter(patients, item => !!size(item?.appointments));
-  const appointments = flatten(patientsWithAppts.map(item => item.appointments));
+  const patientsWithAppts = filter(patients, (item) => !!size(item?.appointments));
+  const appointments = flatten(patientsWithAppts.map((item) => item.appointments));
 
   if (size(appointments)) {
     const exactMatch = [];
@@ -117,7 +47,7 @@ async function suggestedPatients(dispatch, requestData, patientUser, apptWrite) 
       const selectedApp = exactMatch[0];
       const name = `${selectedApp.patient.firstName} ${selectedApp.patient.lastName}`;
 
-      const patientModel = patients.find(patient => patient.id === selectedApp.patientId);
+      const patientModel = patients.find((patient) => patient.id === selectedApp.patientId);
       const patientUserId = patientUser.id;
       const modifiedPatient = {
         ...patientModel,
@@ -138,7 +68,11 @@ async function suggestedPatients(dispatch, requestData, patientUser, apptWrite) 
           alert: alertRequestUpdate(name),
         }),
       );
-    } else if (size(partialMatch)) {
+      dispatch(selectAppointment(null));
+      return patients;
+    }
+
+    if (size(partialMatch)) {
       set(cloneRequestData, 'nextAppt', appointments);
       dispatch(selectAppointment(cloneRequestData));
     }
@@ -180,9 +114,9 @@ function matchPatient(patient, params, appointment, requestData, exactMatch, par
   const { firstName } = params;
   const score = matchPatientScore(patient, params);
   if (
-    inRangeOf(4, 'hours', appointment, requestData)
-    && firstName
-    && lower(firstName) === lower(patient.firstName)
+    inRangeOf(4, 'hours', appointment, requestData) &&
+    firstName &&
+    lower(firstName) === lower(patient.firstName)
   ) {
     if (score >= 3) {
       exactMatch.push({
@@ -231,7 +165,7 @@ function inRangeOf(range, unit, appointment, requestData) {
 }
 
 export function setAllFilters(entityKeys) {
-  return function (dispatch, getState) {
+  return function(dispatch, getState) {
     const { entities } = getState();
 
     entityKeys.forEach((key) => {
