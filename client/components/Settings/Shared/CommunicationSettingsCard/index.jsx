c39e4520d19ff9f7c346e40838e1ec6ff69d4bd9
Siddharth Parmar <siddharthparmar7@gmail.com>
2021-07-27 14:43:43 -0700

    CRU-3517 - fix nav styles (#499)

--- client/components/Settings/Shared/CommunicationSettingsCard/index.jsx
+++ client/components/Settings/Shared/CommunicationSettingsCard/index.jsx
@@ -1,17 +1,14 @@
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Grid, Row, Col } from '../../../library';
-import SettingsCard from '../../Shared/SettingsCard';
+import SettingsCard from '../SettingsCard';
 import styles from './styles.scss';
 
 export default function CommunicationSettingsCard(props) {
-  const {
-    title, rightActions, leftColumn, rightColumn, children,
-  } = props;
+  const { title, rightActions, leftColumn, rightColumn, children } = props;
 
   return (
-    <SettingsCard title={title} rightActions={rightActions}>
+    <SettingsCard title={title} headerClass={styles.displayFlex} rightActions={rightActions}>
       <Grid className={styles.commsGrid}>
         <Row className={styles.commsRow}>
           <Col xs={6} className={styles.commsListCol}>
@@ -26,8 +23,9 @@ export default function CommunicationSettingsCard(props) {
 }
 
 CommunicationSettingsCard.propTypes = {
-  title: PropTypes.string,
-  rightActions: PropTypes.element,
-  leftColumn: PropTypes.element,
-  rightColumn: PropTypes.element,
+  title: PropTypes.string.isRequired,
+  rightActions: PropTypes.element.isRequired,
+  leftColumn: PropTypes.element.isRequired,
+  rightColumn: PropTypes.element.isRequired,
+  children: PropTypes.element.isRequired,
 };
