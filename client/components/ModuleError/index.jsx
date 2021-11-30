57697458f905fbd3197f74adf539be16dca381b8
parkhiCareCru <92784795+parkhiCareCru@users.noreply.github.com>
2021-11-30 14:05:49 +0530

    CRU-4866_UI_Improvements | Implemented Suggested improvements. (#636)
    * CRU-4866_UI_Improvements | Implemented Suggested improvements.
    
    * CRU-4866_UI_Improvements | Removed Donna image for module error.

--- client/components/ModuleError/index.jsx
+++ client/components/ModuleError/index.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import styles from './module-error.scss';
+import { Button } from '../library';
 
 export default function index() {
+  const tryAgainHandler = () => {
+    window.location.reload();
+  };
   return (
     <div className={styles.errorWrapper}>
-      <div>
-        <img className={styles.errorImg} src="/images/ErrorMessage.png" alt="Error!" />
-      </div>
       <div className={styles.header}>Uh oh, something went wrong...</div>
       <div className={styles.supportText}>
         Please try again. If the issue persists, please contact our support team.
       </div>
+      <Button color="blue" rounded onClick={tryAgainHandler} className={styles.retryButton}>
+        Try Again
+      </Button>
       <div className={styles.bottomText}>
-        Contact us at <span className={styles.highlight}>1 (888) 870-0077</span> or{' '}
-        <a href="mailto:support@carecru.com?subject=Bug report" className={styles.highlight}>
-          support@carecru.com
-        </a>
+        <div>
+          Contact us at <span className={styles.highlight}> 1 (888) 870-0077</span> or{' '}
+          <a href="mailto:support@carecru.com?subject=Bug report" className={styles.highlight}>
+            support@carecru.com
+          </a>
+        </div>
       </div>
     </div>
   );
