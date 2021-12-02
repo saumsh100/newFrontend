bbae2ec3bf6e6927dd02b3859878d4b68e24f337
Siddharth Parmar <siddharthparmar7@gmail.com>
2021-12-02 12:50:35 -0800

    CRU-5046/pass feature flag to MFE Forms app  (#650)

--- client/micro-front-ends/settings/forms.jsx
+++ client/micro-front-ends/settings/forms.jsx
@@ -5,10 +5,10 @@ import historyShape from '../../components/library/PropTypeShapes/historyShape';
 import locationShape from '../../components/library/PropTypeShapes/locationShape';
 import userShape from '../../components/library/PropTypeShapes/patientUserShape';
 import AccountModel from '../../entities/models/Account';
+import { isFeatureEnabledSelector } from '../../reducers/featureFlags';
 import MicroFrontEnd from '../micro-front-end';
 
 const { FORMS_HOST: formsHost } = process.env;
-
 
 const Forms = React.memo(({ history, location, match, ...rest }) => {
   delete rest.dispatch; // no need for redux at this time
@@ -33,11 +33,17 @@ Forms.propTypes = {
   match: PropTypes.shape({}).isRequired,
   user: PropTypes.shape(userShape).isRequired,
   role: PropTypes.string.isRequired,
+  useFormSubmission: PropTypes.bool.isRequired,
 };
 
-const mapStateToProps = ({ entities, auth }) => {
+const mapStateToProps = ({ entities, auth, featureFlags }) => {
+  const isDev = process.env.NODE_ENV === 'development';
+  const useFormSubmission = isDev
+    ? true
+    : isFeatureEnabledSelector(featureFlags.get('flags'), 'use-form-submission');
   return {
     activeAccount: entities.getIn(['accounts', 'models', auth.get('accountId')]).toJS(),
+    useFormSubmission,
   };
 };
 
