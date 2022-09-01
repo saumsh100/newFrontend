1f39914f659076ff2348ad8c816aad250dd27524
Pankaj Bharati <87113477+pankajbhccru@users.noreply.github.com>
2022-09-01 15:27:31 +0530

    feat: remove reset password from Add Practice Owner (#1093)
    * feat: remove reset password from Add Practice Owner
    
    * fix: remove unnecessary css

--- client/components/Admin/Enterprises/CreateAccount/AddUser/index.jsx
+++ client/components/Admin/Enterprises/CreateAccount/AddUser/index.jsx
@@ -1,19 +1,15 @@
-
+/* eslint-disable react/require-default-props */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Form, Field } from '../../../../library';
 import {
   emailValidate,
   maxLength,
-  passwordsValidate,
-  passwordStrength,
   asyncEmailValidateUser,
 } from '../../../../library/Form/validate';
 
 export default function AddUser(props) {
-  const {
-    onSubmit, index, initialValues, formName,
-  } = props;
+  const { onSubmit, index, initialValues, formName } = props;
 
   return (
     <Form
@@ -27,39 +23,9 @@ export default function AddUser(props) {
       asyncBlurFields={['email']}
       ignoreSaveButton
     >
-      <Field
-        required
-        validate={[maxLength(25)]}
-        name="firstName"
-        label="First Name"
-      />
-      <Field
-        required
-        validate={[maxLength(25)]}
-        name="lastName"
-        label="Last Name"
-      />
-      <Field
-        required
-        validate={[emailValidate]}
-        type="email"
-        name="email"
-        label="Email"
-      />
-      <Field
-        required
-        type="password"
-        name="password"
-        validate={[passwordsValidate, passwordStrength]}
-        label="Password"
-      />
-      <Field
-        required
-        type="password"
-        name="confirmPassword"
-        validate={[passwordsValidate, passwordStrength]}
-        label="Password Confirmation"
-      />
+      <Field required validate={[maxLength(25)]} name="firstName" label="First Name" />
+      <Field required validate={[maxLength(25)]} name="lastName" label="Last Name" />
+      <Field required validate={[emailValidate]} type="email" name="email" label="Email" />
     </Form>
   );
 }
