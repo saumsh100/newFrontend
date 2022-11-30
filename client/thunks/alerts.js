73684b9f1541f1357924848d2aca3f304003c051
Vitor Felicio <feliciovcm@gmail.com>
2022-11-30 15:31:53 -0300

    CBUG-2797: create check condition to create alert (#1167)
    Co-authored-by: Vitor Felicio Costa Moreira <vitorfelicio@pop-os.localdomain>

--- client/thunks/alerts.js
+++ client/thunks/alerts.js
@@ -1,11 +1,22 @@
-
+/* eslint-disable import/prefer-default-export */
 import { createAlert, removeAlert } from '../reducers/alerts';
+
+function checkExistAlert(payload, alertList) {
+  const { alert = {} } = payload;
+  if (alert && alert.body) {
+    return alertList.some((alertListItem) => alertListItem?.get('body') === alert.body);
+  }
+  return false;
+}
 
 export function showAlertTimeout(payload) {
   return (dispatch, getState) => {
-    dispatch(createAlert(payload));
-
     const alerts = getState().alerts ? getState().alerts.toArray() : [];
+
+    const hasAlertDisplayed = checkExistAlert(payload, alerts);
+    if (!hasAlertDisplayed) {
+      dispatch(createAlert(payload));
+    }
 
     alerts.map((alert) => {
       if (alert && !alert.get('sticky')) {
@@ -13,6 +24,7 @@ export function showAlertTimeout(payload) {
           dispatch(removeAlert({ alert }));
         }, alert.time);
       }
+      return null;
     });
   };
 }
