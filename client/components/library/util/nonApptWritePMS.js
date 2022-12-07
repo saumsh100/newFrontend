e803add5f7462b19c57765f74dbed29b76f94f0e
Vitor Felicio <feliciovcm@gmail.com>
2022-12-07 11:46:32 -0300

    CRU-8278: allow appointment write into adapter type dentrix v80 (#1177)
    Co-authored-by: Vitor Felicio Costa Moreira <vitorfelicio@pop-os.localdomain>

--- client/components/library/util/nonApptWritePMS.js
+++ client/components/library/util/nonApptWritePMS.js
@@ -1,13 +1,14 @@
-
 const NON_APPT_WRITE_PMS = ['dentrix'];
-
+const DENTRIX_APPT_WRITE_PMS = ['DENTRIX_ENTERPRISE_V80'];
 /**
  * Check if PMS is non appointment write,
  * for now, any version of Dentrix not able to write.
  */
 export const nonApptWritePMS = (adapterType) => {
+  const dentrixException = DENTRIX_APPT_WRITE_PMS.find((name) => name === adapterType);
+  if (dentrixException) return !dentrixException;
   const type = String(adapterType).toLowerCase();
-  return !!NON_APPT_WRITE_PMS.find(name => type?.includes(name));
+  return !!NON_APPT_WRITE_PMS.find((name) => type?.includes(name));
 };
 
 export default nonApptWritePMS;
